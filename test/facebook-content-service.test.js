ea8c9821600671b53476875bfe258cfb874be963
Alex Stelea <alex.stelea@gmail.com>
2014-03-18 18:19:21 -0400

    Added initial facebook content

---
+++ test/facebook-content-service.test.js
@@ -0,0 +1,21 @@
+var expect = require('chai').expect;
+var should = require('chai').should();
+var FacebookContentService = require('../controllers/fetching/content-services/fb-content-service');
+
+describe('Facebook content service', function() {
+    before(function(done) {
+        facebookContentService = new FacebookContentService(null, 'prezicom');
+        done();
+    });
+
+    it('should fetch content from Facebook', function(done) {
+        var data = facebookContentService.parse();
+        should.exist(data);
+        data.should.be.a('string');
+        facebookContentService.should.have.property('lastCrawlDate');
+        // Stop stream to ensure a single fetch
+        facebookContentService.setCrawlDate("2013");
+        (facebookContentService.should.have.property('lastCrawlDate')).to.equal("2013");
+        done();
+    });
+});
