c02903ec1de3bba9a4a5622f12902690852aeb59
Alex Stelea <alex.stelea@gmail.com>
2014-03-22 20:43:58 -0400

    FB Graph Magic

--- test/facebook-content-service.test.js
+++ test/facebook-content-service.test.js
@@ -4,14 +4,14 @@ var FacebookContentService = require('../controllers/fetching/content-services/f
 
 describe('Facebook content service', function() {
     before(function(done) {
-        facebookContentService = new FacebookContentService({lastCrawlDate: undefined, fbPage:'prezicom'});
+        facebookContentService = new FacebookContentService({lastCrawlDate: ((+new Date(2014,0,1))/1000), untilCrawlDate: ((+new Date(2014,3,2))/1000), fbPage:'prezicom'});
         done();
     });
 
     it('should fetch content from Facebook', function(done) {
         var data = facebookContentService.fetch();
-        // should.exist(data);
-        // data.should.be.a('string');
+        should.exist(data);
+        data.should.be.a('string').to.equal('done');
         // facebookContentService.should.have.property('lastCrawlDate');
         // // Stop stream to ensure a single fetch
         // facebookContentService.setCrawlDate("2013");
