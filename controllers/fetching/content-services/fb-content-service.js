fa212ad158d1c097f6ab9156c43958c19f45753d
Alex Stelea <alex.stelea@gmail.com>
2014-03-22 20:47:16 -0400

    Some styling improvements

--- controllers/fetching/content-services/fb-content-service.js
+++ controllers/fetching/content-services/fb-content-service.js
@@ -3,12 +3,11 @@ var ContentService = require('../content-service');
 var graph = require('fbgraph');
 var util = require('util');
 var async = require('async');
-var cp = require('child_process');
-var requests = require('request');
 
 var FacebookContentService = function(options) {
 
     graph.setAccessToken(config.accessToken);
+
     if (options.lastCrawlDate) {
         this.lastCrawlDate = options.lastCrawlDate;
     } else {
@@ -23,7 +22,7 @@ var FacebookContentService = function(options) {
     this.source = 'facebook';
     this.resultsPerFetch = 100;
     this._isStreaming = false;
-    this.next = undefined;
+    this.nextPagingItem = undefined;
     this.untilCrawlDate = options.untilCrawlDate;
 
 };
@@ -34,7 +33,7 @@ FacebookContentService.prototype.setCrawlDate = function(crawlDate) {
 
 FacebookContentService.prototype.fetch = function() {
 
-    //This will need to be changed into an array 
+    //Saving the instance of fbContentService
     var fbContentService = this;
 
     var options = {
@@ -42,71 +41,64 @@ FacebookContentService.prototype.fetch = function() {
         limit: this.resultsPerFetch,
         until: this.untilCrawlDate,
     };
-    if (this.next) {
+
+    //here we check if there is a next page 
+    if (this.nextPagingItem) {
         options = {
             since: this.lastCrawlDate,
             limit: this.resultsPerFetch,
-            until: this.next,
+            until: this.nextPagingItem,
         };
     }
-    console.log(options);
+
+    // console.log(options);
     graph
         .get(this.fbPage + '/feed/', options, function(err, res) {
-
-
             if (err) {
                 console.error(err);
             }
 
             //messages array
             messagesOnFeed = [];
-
             //comments array
             commentsOnPost = [];
-
             //length of array
             var responseLength = res.data.length;
 
             // var newestPost = null;
             for (var i = responseLength - 1; i >= 0; i--) {
-
+                
                 var postHasData = res.data[i];
                 var postHasComments = (res.data[i].comments);
-
 
                 //check if date is valid for each post
                 var validDate = (new Date(res.data[i].created_time) <= new Date(fbContentService.untilCrawlDate * 1000));
 
                 if (postHasData && validDate) {
                     messagesOnFeed.push(postHasData);
-
                 }
                 if (postHasComments && validDate) {
                     commentsOnPost.push(postHasComments.data[0]);
                 }
             }
 
+            //helper method to remove url param
             function getURLParameter(name, url) {
                 return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(url) || [, ""])[1].replace(/\+/g, '%20')) || null;
             }
 
-
             if (res.paging && res.paging.next) {
-
 
                 //This part is very confusing, paging.next gives me a previus entry
                 fbContentService.next = getURLParameter('until', res.paging.next);
-                console.log(fbContentService.next);
-
+                // console.log(fbContentService.next);
 
                 async.parallel([
                         function(callback) {
                             var content = fbContentService.parse(messagesOnFeed, commentsOnPost);
                             callback(null, 'content saved');
                             return content;
-
                         },
-
                         function(callback) {
                             fbContentService.fetch();
                             callback(null, 'Fetched new source');
@@ -117,7 +109,6 @@ FacebookContentService.prototype.fetch = function() {
                         return results;
 
                     });
-
             }
         });
 
