ce1c1ac11d54d7568a999830983e9dff8a39c5a1
Victor Kareh <vkareh@vkareh.net>
2014-04-09 10:24:45 -0400

    Wait for report processing before testing report controller.

--- test/controllers.api.report-controller.test.js
+++ test/controllers.api.report-controller.test.js
@@ -4,6 +4,7 @@ var request = require('supertest');
 var _ = require('underscore');
 var reportController = require('../controllers/api/report-controller');
 var botMaster = require('../controllers/fetching/bot-master');
+var reportWriter = require('../controllers/fetching/report-writer');
 var Source = require('../models/source');
 
 describe('Report controller', function() {
@@ -19,8 +20,11 @@ describe('Report controller', function() {
   // Stream data for 100ms
   before(function(done) {
     setTimeout(function() {
+      // Wait until all reports have been processed
+      reportWriter.once('done', function() {
+        done();
+      });
       botMaster.stop();
-      done();
     }, 100);
   });
 
