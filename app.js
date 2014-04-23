8b995288d853136cd69fe293a7e2285f14ce17a9
Victor Kareh <vkareh@vkareh.net>
2014-04-23 12:03:51 -0400

    Remove empty module.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var processManager = require('./controllers/process-manager');
 processManager.fork('/controllers/api');
 processManager.fork('/controllers/fetching');
-processManager.fork('/controllers/streaming');
 
 module.exports = processManager;
