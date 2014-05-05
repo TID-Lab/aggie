c5db4d60a0851eb79907fe55ea51d8884764c64d
Cory Kaufman-Schofield <cory@corykaufman.com>
2014-05-05 17:53:48 -0400

    Tidied up styles a bit

--- public/angular/js/app.js
+++ public/angular/js/app.js
@@ -1,32 +1,41 @@
 var aggie = angular.module('Aggie', ['ui.router']);
+
 aggie.config(["$stateProvider", "$urlRouterProvider", "$locationProvider",
   function($stateProvider, $urlRouterProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
+    // $locationProvider.html5Mode(true);
+
     $urlRouterProvider.otherwise('/');
-    $stateProvider.state('reports', {
+
+    $stateProvider.state('home', {
+      url: '/',
+      templateUrl: '/templates/home.html',
+    }).state('reports', {
       url: '/reports',
-      templateUrl: 'templates/reports.html',
+      templateUrl: '/templates/reports.html',
       controller: function($scope) {
         $scope.items = ['A', 'List', 'Of', 'Reports'];
       }
     }).state('incidents', {
       url: '/incidents',
-      templateUrl: 'templates/incidents.html',
+      templateUrl: '/templates/incidents.html',
       controller: function($scope){
         $scope.things = ['A', 'List', 'Of', 'Incidents'];
       }
     }).state('sources', {
       url: '/sources',
-      templateUrl: 'templates/sources.html',
+      templateUrl: '/templates/sources.html',
       controller: function($scope){
         $scope.things = ['A', 'List', 'Of', 'Sources'];
       }
     }).state('analysis', {
       url: '/analysis',
-      templateUrl: 'templates/analysis.html'
+      templateUrl: '/templates/analysis.html'
     }).state('settings', {
       url: '/settings',
-      templateUrl: 'templates/settings.html'
+      templateUrl: '/templates/settings.html'
+    }).state('user', {
+      url: '/user',
+      templateUrl: '/templates/user.html'
     });
   }
 ]);
