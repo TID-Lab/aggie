ae8e5e214922373d87b2ea28595dd886806df4cc
Alec Tower <alectower@gmail.com>
2014-05-07 19:28:54 -0400

    Added rootScope to password reset

--- public/angular/js/controllers/password_reset.js
+++ public/angular/js/controllers/password_reset.js
@@ -1,10 +1,11 @@
 angular.module('Aggie')
   .controller('PasswordResetController', [
+    '$rootScope',
     '$scope',
     '$location',
     '$http',
     'AuthService',
-    function($scope, $location, $http, AuthService) {
+    function($rootScope, $scope, $location, $http, AuthService) {
       $scope.alerts = [];
 
       var passwordsMatch = function() {
