300a3d47c85febea333db8f1f596d18fc3fa16a2
Cory Kaufman-Schofield <cory@corykaufman.com>
2014-05-15 16:59:28 -0400

    Retrieve total # reports via API

--- public/angular/js/services/factories.js
+++ public/angular/js/services/factories.js
@@ -1,7 +1,10 @@
 angular.module('Aggie')
 
 .factory('Report', function($resource) {
-  return $resource("/api/v1/report/:id", { id: '@_id' }, { 'save': { method: 'PUT' } });
+  return $resource("/api/v1/report/:id", { id: '@_id' }, {
+    'query': { isArray: false },
+    'save': { method: 'PUT' }
+  });
 })
 
 .factory('Source', function($resource) {
