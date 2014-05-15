6d626edd0b67fefb18845f48410478a8fef802a8
Cory Kaufman-Schofield <cory@corykaufman.com>
2014-05-15 16:30:28 -0400

    Add source toggle

--- public/angular/js/filters/report.js
+++ public/angular/js/filters/report.js
@@ -1,14 +1,18 @@
-var human = require('interval-to-human');
+var human = require('interval-to-human'),
+  abbreviatedHuman = function(time, unit) {
+    return human(time, unit).replace(/(\w)\w+$/, "$1");
+  }
+
 angular.module('Aggie')
 
 .filter('delay', function() {
   return function(record) {
     var stored = new Date(record.storedAt).getTime();
     var authored = new Date(record.authoredAt).getTime();
-    var delay = stored - authored
-    var minute = 60000;
+    var delay = stored - authored;
+    var minute = 60 * 1000;
     if (delay > minute) {
-      return 'Created +' + human(delay);
+      return 'Created +' + abbreviatedHuman(delay);
     }
     return '';
   };
@@ -18,6 +22,6 @@ angular.module('Aggie')
   return function(time) {
     var now = new Date().getTime();
     var then = new Date(time).getTime();
-    return human(now - then);
+    return abbreviatedHuman(now - then);
   };
 });
--- public/angular/js/services/factories.js
+++ public/angular/js/services/factories.js
@@ -1,7 +1,7 @@
 angular.module('Aggie')
 
 .factory('Report', function($resource) {
-  return $resource("/api/v1/report/:id");
+  return $resource("/api/v1/report/:id", { id: '@_id' }, { 'save': { method: 'PUT' } });
 })
 
 .factory('Source', function($resource) {
