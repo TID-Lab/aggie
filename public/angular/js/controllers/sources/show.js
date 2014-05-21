37de837987d7a16b62cc0bb9b9abb3390fc323ea
Alec Tower <alectower@gmail.com>
2014-05-21 10:29:03 -0400

    Delete source

--- public/angular/js/controllers/sources/show.js
+++ public/angular/js/controllers/sources/show.js
@@ -2,11 +2,23 @@ angular.module('Aggie')
 
 .controller('SourcesShowController', [
   '$scope',
+  '$rootScope',
   '$stateParams',
   'Source',
   'source',
-  function($scope, $stateParams, Source, source) {
+  'FlashService',
+  function($scope, $rootScope, $stateParams, Source, source, flash) {
     $scope.source = source;
     Source.resetUnreadErrorCount({ id: source._id }, source);
+
+    $scope.delete = function() {
+      Source.delete({id: $scope.source._id}, function(){
+        flash.setNotice('Source was successfully deleted, ' +
+          'but reports related to this source will still remain.');
+         $rootScope.$state.go('sources');
+      }, function() {
+        flash.setAlertNow('Source failed to be deleted.');
+      });
+    };
   }
 ]);
