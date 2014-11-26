8b93cd5299f8d79f288d88045fd08a8e641e6aa5
Tom Smyth <tom@sassafras.coop>
2014-11-26 15:46:58 -0500

    Fixed elmo tests, removed tests that rely upon outside elmo instance

--- test/lib.fetching.content-services.elmo-content-service.test.js
+++ test/lib.fetching.content-services.elmo-content-service.test.js
@@ -48,11 +48,11 @@ describe('ELMO content service', function() {
       expect(report_data).to.have.property('author');
       switch (remaining) {
         case 2:
-          expect(report_data.content).to.contain('[CodeFOO: Certainly] [CodeBAR: Nope] [CodeBAZ: Perhaps]');
+          expect(report_data.content).to.contain('[FOO: Certainly] [BAR: Nope] [BAZ: Perhaps]');
           expect(report_data.author).to.equal(1);
           break;
         case 1:
-          expect(report_data.content).to.contain('[CodeFOO2: Yes] [CodeBAR2: No] [CodeBAZ2: Maybe]');
+          expect(report_data.content).to.contain('[FOO2: Yes] [BAR2: No] [BAZ2: Maybe]');
           expect(report_data.author).to.equal(2);
           break;
       }
@@ -79,7 +79,7 @@ describe('ELMO content service', function() {
       expect(report_data).to.have.property('author');
       switch (remaining) {
         case 1:
-          expect(report_data.content).to.contain('[CodeFOO3: Affirmative] [CodeBAR3: Negative] [CodeBAZ3: Doubtful]');
+          expect(report_data.content).to.contain('[FOO3: Affirmative] [BAR3: Negative] [BAZ3: Doubtful]');
           expect(report_data.author).to.equal(3);
           break;
       }
@@ -93,38 +93,12 @@ describe('ELMO content service', function() {
     elmoContentService.once('error', done);
   });
 
-  describe('Online', function() {
+  describe('Errors', function() {
     before(function() {
       // Restore original `fetch()`
       ELMOContentService.prototype.fetch = _fetch;
-      elmoContentService = new ELMOContentService({url: 'https://secure1.sassafras.coop/api/v1/responses.json?form_id=19'});
     });
 
-    it('should fetch real content from ELMO', function(done) {
-      elmoContentService.fetch(function(err, lastReportDate) {
-      expect(err).to.be.null;
-      expect(lastReportDate).to.exist;
-    });
-      elmoContentService.once('report', function(report_data) {
-        expect(report_data).to.have.keys(['authoredAt', 'fetchedAt', 'content', 'author']);
-        expect(report_data.content).to.contain('[Code');
-        done();
-      });
-      elmoContentService.on('error', done);
-    });
-
-    it('should re-fetch to get an empty content list', function(done) {
-      elmoContentService.fetch(function(err, lastReportDate) {
-        expect(err).to.be.null;
-        expect(lastReportDate).to.exist;
-      });
-      expectToNotEmitReport(elmoContentService, done);
-      elmoContentService.once('error', done);
-      setTimeout(done, 500);
-    });
-  });
-
-  describe('Errors', function() {
     it('should emit a missing URL error', function(done) {
       var elmoContentService = new ELMOContentService({});
       elmoContentService.fetch();
@@ -134,9 +108,15 @@ describe('ELMO content service', function() {
 
     it('should emit an unauthorized token error', function(done) {
       elmoContentService = new ELMOContentService({
-        url: 'https://secure1.sassafras.coop/api/v1/responses.json?form_id=19',
+        url: 'https://example.com',
         authToken: '123'
       });
+
+      // Stub the content service to return 403
+      elmoContentService._httpRequest = function(params, callback) {
+        process.nextTick(function() { callback({message: 'Unauthorized'}); });
+      };
+
       elmoContentService.fetch();
       expectToNotEmitReport(elmoContentService, done);
       expectToEmitError(elmoContentService, 'Unauthorized', done);
