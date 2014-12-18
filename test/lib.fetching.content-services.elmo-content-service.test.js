afb28cd5ae70ed79db203128fcbe17ee023effb3
Amandeep Sapra <sapraaman@gmail.com>
2014-12-18 16:15:55 +0530

    #1432 Content services refactoring to have common code in base class

--- test/lib.fetching.content-services.elmo-content-service.test.js
+++ test/lib.fetching.content-services.elmo-content-service.test.js
@@ -1,45 +1,56 @@
 require('./init');
+var util = require('util');
 var expect = require('chai').expect;
 var ELMOContentService = require('../lib/fetching/content-services/elmo-content-service');
 var ContentService = require('../lib/fetching/content-service');
 
-var elmoContentService, _fetch;
-describe('ELMO content service', function() {
-  before(function(done) {
-    // Override fetch so that we can test local files
-    _fetch = ELMOContentService.prototype.fetch;
-    ELMOContentService.prototype.fetch = function(fixture, callback) {
+function loadElmoServiceWithFixture(service, fixture) {
+  var elmoContentService = service;
+  if (!elmoContentService) {
+    var FixtureBasedElmoContentService = function(options) {
+      ELMOContentService.call(this, options);
+    };
+    
+    util.inherits(FixtureBasedElmoContentService, ELMOContentService);
+    elmoContentService = new FixtureBasedElmoContentService({url: 'http://dummy_url'});
+  
+    FixtureBasedElmoContentService.prototype._loadData = function(options, cb) {
       var self = this;
-      var data = require(fixture);
+      var data = require(options.fixture);
       process.nextTick(function() {
         if (data.length) self._handleResults(data);
-        callback && callback(null, self.lastReportDate);
+        return cb(null, self._lastReportDate);
       });
     };
-    elmoContentService = new ELMOContentService({});
-    done();
-  });
+  }
+  
+  var options = {fixture: fixture};
+  elmoContentService.fetch(options);
+  
+  return elmoContentService;
+}
 
+describe('ELMO content service', function() {
+  
   it('should instantiate correct ELMO content service', function() {
+    var elmoContentService = new ELMOContentService({});
     expect(elmoContentService).to.be.instanceOf(ContentService);
     expect(elmoContentService).to.be.instanceOf(ELMOContentService);
   });
-
+  
   it('should fetch empty content', function(done) {
-    elmoContentService.fetch('./fixtures/elmo-0.json', function(err, lastReportDate) {
-      expect(err).to.be.null;
-      expect(lastReportDate).to.be.null;
-    });
+    var elmoContentService = loadElmoServiceWithFixture(null, './fixtures/elmo-0.json');
     expectToNotEmitReport(elmoContentService, done);
-    elmoContentService.once('error', done);
+    expect(elmoContentService._lastReportDate).to.be.null;
+    elmoContentService.once('error', function(err) {
+      done(err);
+    });
+    
     setTimeout(done, 500);
   });
-
+  
   it('should fetch mock content from ELMO', function(done) {
-    elmoContentService.fetch('./fixtures/elmo-1.json', function(err, lastReportDate) {
-      expect(err).to.be.null;
-      expect(lastReportDate).to.equal(Date.parse('2014-06-17T15:00:00Z'));
-    });
+    var elmoContentService = loadElmoServiceWithFixture(null, './fixtures/elmo-1.json');
     var remaining = 2;
     elmoContentService.on('report', function(report_data) {
       expect(report_data).to.have.property('fetchedAt');
@@ -59,46 +70,67 @@ describe('ELMO content service', function() {
       if (--remaining === 0) {
         // Wait so that we can catch unexpected reports
         setTimeout(done, 100);
+        expect(elmoContentService._lastReportDate).to.equal(Date.parse('2014-06-17T15:00:00Z'));
       } else if (remaining < 0) {
         return done(new Error('Unexpected report'));
       }
     });
-    elmoContentService.once('error', done);
+    elmoContentService.once('error', function(err) {
+      done(err);
+    });
   });
-
+  
   it('should query for new data without duplicates', function(done) {
-    elmoContentService.fetch('./fixtures/elmo-2.json', function(err, lastReportDate) {
-      expect(err).to.be.null;
-      expect(lastReportDate).to.equal(Date.parse('2014-06-17T20:00:00Z'));
+    
+    function onFixture1ReportCallback(cb) {
+      var remaining = 2;
+      return function(report_data) {
+        switch (remaining--) {
+          case 2:
+            expect(report_data.content).to.contain('[FOO: Certainly] [BAR: Nope] [BAZ: Perhaps]');
+            expect(report_data.author).to.equal(1);
+            break;
+          case 1:
+            expect(report_data.content).to.contain('[FOO2: Yes] [BAR2: No] [BAZ2: Maybe]');
+            expect(report_data.author).to.equal(2);
+            
+            setTimeout(function() {
+              expect(elmoContentService._lastReportDate).to.equal(Date.parse('2014-06-17T15:00:00Z'));
+              cb();
+            }, 100);
+            break;
+        }
+      };
+    }
+    
+    function onFixture2ReportCallback(cb) {
+      return function(report_data) {
+        expect(report_data.content).to.contain('[FOO3: Affirmative] [BAR3: Negative] [BAZ3: Doubtful]');
+        expect(report_data.author).to.equal(3);
+        cb();
+      };
+    }
+    
+    var elmoContentService = loadElmoServiceWithFixture(null, './fixtures/elmo-1.json');
+    elmoContentService.once('error', function(err) {
+      done(err);
     });
-    var remaining = 1;
-    elmoContentService.on('report', function(report_data) {
-      expect(report_data).to.have.property('fetchedAt');
-      expect(report_data).to.have.property('authoredAt');
-      expect(report_data).to.have.property('content');
-      expect(report_data).to.have.property('author');
-      switch (remaining) {
-        case 1:
-          expect(report_data.content).to.contain('[FOO3: Affirmative] [BAR3: Negative] [BAZ3: Doubtful]');
-          expect(report_data.author).to.equal(3);
-          break;
-      }
-      if (--remaining === 0) {
-        // Wait so that we can catch duplicate reports
-        setTimeout(done, 100);
-      } else if (remaining < 0) {
-        return done(new Error('Duplicate report'));
-      }
-    });
-    elmoContentService.once('error', done);
+    
+    elmoContentService.on('report', onFixture1ReportCallback(function(err){
+      if (err) return done(err);
+      
+      elmoContentService.removeAllListeners('report');
+      process.nextTick(function() {
+        elmoContentService.once('report', onFixture2ReportCallback(function(err){
+          return done(err);
+        }));
+        loadElmoServiceWithFixture(elmoContentService, './fixtures/elmo-2.json');
+      });
+    }));
   });
-
+  
   describe('Errors', function() {
-    before(function() {
-      // Restore original `fetch()`
-      ELMOContentService.prototype.fetch = _fetch;
-    });
-
+    
     it('should emit a missing URL error', function(done) {
       var elmoContentService = new ELMOContentService({});
       elmoContentService.fetch();
@@ -121,9 +153,5 @@ describe('ELMO content service', function() {
       expectToNotEmitReport(elmoContentService, done);
       expectToEmitError(elmoContentService, 'Unauthorized', done);
     });
-  });
-
-  afterEach(function() {
-    elmoContentService.removeAllListeners();
   });
 });
--- test/lib.fetching.content-services.facebook-content-service.test.js
+++ test/lib.fetching.content-services.facebook-content-service.test.js
@@ -1,32 +1,52 @@
 require('./init');
+var util = require('util');
 var expect = require('chai').expect;
 var FacebookContentService = require('../lib/fetching/content-services/facebook-content-service');
 var ContentService = require('../lib/fetching/content-service');
 
-var facebookContentService, _fetch;
-describe('Facebook content service', function() {
-  before(function(done) {
-    // Override fetch so that we can test local files
-    _fetch = FacebookContentService.prototype.fetch;
-    FacebookContentService.prototype.fetch = function(fixture, callback) {
+function loadFbServiceWithFixture(service, fixture) {
+  var facebookContentService = service;
+  if (!facebookContentService) {
+    var FixtureBasedFacebookContentService = function(options) {
+      FacebookContentService.call(this, options);
+    };
+    
+    util.inherits(FixtureBasedFacebookContentService, FacebookContentService);
+  
+    FixtureBasedFacebookContentService.prototype._loadData = function(options, cb) {
       var self = this;
-      var data = require(fixture);
+      var data = require(options.fixture);
       process.nextTick(function() {
         self._handleResults(data.posts, data.authors);
-        callback && callback(null, self.lastReportDate * 1000);
+        return cb(null, self._lastReportDate * 1000);
       });
     };
-    facebookContentService = new FacebookContentService({});
-    done();
-  });
+    
+    facebookContentService = new FixtureBasedFacebookContentService({url: 'http://dummy_url'});
+  }
+  
+  var options = {fixture: fixture};
+  facebookContentService.fetch(options);
+  
+  return facebookContentService;
+}
 
+function loadFbServiceWithUrl(url) {
+  var facebookContentService = new FacebookContentService({url: url});
+  facebookContentService.fetch();
+  return facebookContentService;
+}
+
+describe('Facebook content service', function() {
+  
   it('should instantiate correct facebook content service', function() {
+    var facebookContentService = new FacebookContentService({});
     expect(facebookContentService).to.be.instanceOf(ContentService);
     expect(facebookContentService).to.be.instanceOf(FacebookContentService);
   });
-
+  
   it('should fetch empty content', function(done) {
-    facebookContentService.fetch('./fixtures/facebook-0.json');
+    var facebookContentService = loadFbServiceWithFixture(null, './fixtures/facebook-0.json');
     expectToNotEmitReport(facebookContentService, done);
     facebookContentService.once('error', function(err) {
       done(err);
@@ -35,9 +55,9 @@ describe('Facebook content service', function() {
       done();
     }, 100);
   });
-
+  
   it('should fetch mock content from Facebook', function(done) {
-    facebookContentService.fetch('./fixtures/facebook-1.json');
+    var facebookContentService = loadFbServiceWithFixture(null, './fixtures/facebook-1.json');
     var remaining = 3;
     facebookContentService.on('report', function(report_data) {
       expect(report_data).to.have.property('fetchedAt');
@@ -75,56 +95,84 @@ describe('Facebook content service', function() {
       done(err);
     });
   });
-
+  
   it('should query for new data without duplicates', function(done) {
-    facebookContentService.fetch('./fixtures/facebook-2.json');
-    var remaining = 3;
-    facebookContentService.on('report', function(report_data) {
-      expect(report_data).to.have.property('fetchedAt');
-      expect(report_data).to.have.property('authoredAt');
-      expect(report_data).to.have.property('content');
-      expect(report_data).to.have.property('author');
-      expect(report_data).to.have.property('url');
-      switch (remaining) {
-        case 3:
-          expect(report_data.content).to.contain('Go jackets');
-          expect(report_data.author).to.equal('Test User 1');
-          expect(report_data.url).to.contain('comment');
-          break;
-        case 2:
-          expect(report_data.content).to.contain('we can do that');
-          expect(report_data.author).to.equal('Test User 2');
-          expect(report_data.url).to.contain('comment');
-          break;
-        case 1:
-          expect(report_data.content).to.contain('GA Tech ranked');
-          expect(report_data.author).to.equal('Test User 3');
-          expect(report_data.url).to.not.contain('comment');
-          break;
-      }
-      if (--remaining === 0) {
-        // Wait so that we can catch duplicate reports
-        setTimeout(function() {
-          done();
-        }, 100);
-      } else if (remaining < 0) {
-        return done(new Error('Duplicate report'));
-      }
-    });
+    function onFixture1ReportCallback(cb) {
+      var remaining = 3;
+      return function(report_data) {
+        switch (remaining--) {
+          case 3:
+            expect(report_data.content).to.contain('The ACC Champion');
+            expect(report_data.author).to.equal('Georgia Tech');
+            expect(report_data.url).to.not.contain('comment');
+            break;
+          case 2:
+            expect(report_data.content).to.contain('Bioengineers at');
+            expect(report_data.author).to.equal('Georgia Tech');
+            expect(report_data.url).to.not.contain('comment');
+            break;
+          case 1:
+            expect(report_data.content).to.contain('Amazing');
+            expect(report_data.author).to.equal('Test User 1');
+            expect(report_data.url).to.contain('comment');
+            
+            setTimeout(function() {
+              cb();
+            }, 100);
+            break;
+        }
+      };
+    }
+    
+    function onFixture2ReportCallback(cb) {
+      var remaining = 3;
+      return function(report_data) {
+        switch (remaining--) {
+          case 3:
+            expect(report_data.content).to.contain('Go jackets');
+            expect(report_data.author).to.equal('Test User 1');
+            expect(report_data.url).to.contain('comment');
+            break;
+          case 2:
+            expect(report_data.content).to.contain('we can do that');
+            expect(report_data.author).to.equal('Test User 2');
+            expect(report_data.url).to.contain('comment');
+            break;
+          case 1:
+            expect(report_data.content).to.contain('GA Tech ranked');
+            expect(report_data.author).to.equal('Test User 3');
+            expect(report_data.url).to.not.contain('comment');
+            
+            setTimeout(function() {
+              cb();
+            }, 100);
+            break;
+        }
+      };
+    }
+    
+    var facebookContentService = loadFbServiceWithFixture(null, './fixtures/facebook-1.json');
     facebookContentService.once('error', function(err) {
       done(err);
     });
+    
+    facebookContentService.on('report', onFixture1ReportCallback(function(err){
+      if (err) return done(err);
+      
+      facebookContentService.removeAllListeners('report');
+      process.nextTick(function() {
+        facebookContentService.on('report', onFixture2ReportCallback(function(err){
+          return done(err);
+        }));
+        loadFbServiceWithFixture(facebookContentService, './fixtures/facebook-2.json');
+      });
+    }));
   });
-
+  
   describe('Online', function() {
-    before(function() {
-      // Restore original fetch()
-      FacebookContentService.prototype.fetch = _fetch;
-      facebookContentService = new FacebookContentService({url: 'http://www.facebook.com/georgiatech'});
-    });
-
+    
     it('should fetch real content from Facebook', function(done) {
-      facebookContentService.fetch();
+      var facebookContentService = loadFbServiceWithUrl('http://www.facebook.com/georgiatech');
       facebookContentService.once('report', function(report_data) {
         expect(report_data).to.have.property('fetchedAt');
         expect(report_data).to.have.property('authoredAt');
@@ -139,18 +187,25 @@ describe('Facebook content service', function() {
     });
 
     it('should re-fetch to get an empty content list', function(done) {
-      facebookContentService.fetch();
-      expectToNotEmitReport(facebookContentService, done);
-      facebookContentService.once('error', function(err) {
-        done(err);
+      var facebookContentService = loadFbServiceWithUrl('http://www.facebook.com/georgiatech');
+      facebookContentService.once('report', function(report_data) {
+        
+        process.nextTick(function() {
+          facebookContentService.fetch();
+          expectToNotEmitReport(facebookContentService, done);
+          facebookContentService.once('error', function(err) {
+            done(err);
+          });
+          setTimeout(function() {
+            done();
+          }, 100);
+        });
       });
-      setTimeout(function() {
-        done();
-      }, 100);
     });
   });
 
   describe('Errors', function() {
+    
     it('should emit a missing URL error', function(done) {
       var facebookContentService = new FacebookContentService({url: ''});
       facebookContentService.fetch();
@@ -164,9 +219,5 @@ describe('Facebook content service', function() {
       expectToNotEmitReport(facebookContentService, done);
       expectToEmitError(facebookContentService, 'Invalid Facebook URL', done);
     });
-  });
-
-  afterEach(function() {
-    facebookContentService.removeAllListeners();
   });
 });
