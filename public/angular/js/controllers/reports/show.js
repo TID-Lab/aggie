1b8ca07835fb8888cc8738f34146d68ad7fc3430
Michal Kuklis <michal.kuklis@gmail.com>
2015-01-06 22:12:50 -0500

    #1407 - setup loading indicator; refactored router

--- public/angular/js/controllers/reports/show.js
+++ public/angular/js/controllers/reports/show.js
@@ -3,15 +3,9 @@ angular.module('Aggie')
 .controller('ReportsShowController', [
   '$scope',
   '$stateParams',
-  'Report',
-  'Source',
-  function($scope, $stateParams, Report, Source) {
-    Report.get({id: $stateParams.id}, function(report) {
-      $scope.report = report;
-      $scope.report.content = Autolinker.link( report.content );
-      Source.get({id: report._source}, function(source) {
-        $scope.source = source;
-      });
-    });
+  'data',
+  function($scope, $stateParams, data) {
+    $scope.report = data.report;
+    $scope.source = data.source;
   }
 ]);
