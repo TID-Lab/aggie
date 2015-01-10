cd59a6dd8c71fe4a1df5205c06660516c69b0dbc
David Brooks <david@colab.coop>
2015-01-10 16:28:07 -0600

    Feature #1735 New settings page, no more fetching streaming

--- public/angular/js/controllers/fetching.js
+++ public/angular/js/controllers/fetching.js
@@ -2,60 +2,23 @@ angular.module('Aggie')
 
 .controller('FetchingController', [
   '$scope',
-  '$rootScope',
   'Fetching',
-  'Socket',
-  function($scope, $rootScope, Fetching, Socket) {
-    $scope.fetchStatus = null;
+  function($scope, Fetching) {
 
-    var serverFetchStatus = null,
-      stopWatchingFetchStatus;
+    Fetching.get(function success (data) {
+      $scope.on = data;
+    }, failure);
 
-    var init = function() {
-      if ($rootScope.currentUser) {
-        Fetching.get(setFetchStatus);
-        Socket.on('fetchingStatusUpdate', fetchingStatusUpdate);
-        stopWatchingFetchStatus = $scope.$watch('fetchStatus', fetchStatusChanged);
-      } else {
-        Socket.off('fetchingStatusUpdate');
-        if (stopWatchingFetchStatus) {
-          stopWatchingFetchStatus();
-          stopWatchingFetchStatus = null;
-        }
-      }
+    $scope.toggle = function (x) {
+      $scope.on = x;
     };
 
-    var parseStatus = function(status) {
-      if (typeof status == 'string') {
-        return status === 'true';
-      } else {
-        return !!status;
-      }
+    $scope.save = function () {
+      Fetching.set($scope.on, null, failure);
     };
 
-    var fetchingStatusUpdate = function(data) {
-      var oldStatus = parseStatus($scope.fetchStatus),
-        newStatus = parseStatus(data.fetching);
-      if (newStatus === oldStatus) { return }
-      serverFetchStatus = newStatus;
-      $scope.fetchStatus = newStatus;
-    };
-
-    var fetchStatusChanged = function(newStatus, oldStatus) {
-      newStatus = parseStatus(newStatus);
-      oldStatus = parseStatus(oldStatus);
-      if (newStatus === oldStatus || newStatus == serverFetchStatus) { return }
-      Fetching.set(newStatus, function() {
-        serverFetchStatus = newStatus;
-      });
-    };
-
-    var setFetchStatus = function(fetchStatus) {
-      $scope.fetchStatus = parseStatus(fetchStatus);
-    };
-
-    $rootScope.$watch('currentUser', init);
-
-    init();
+    function failure (data) {
+      console.log('failure: ', data);
+    }
   }
 ]);
