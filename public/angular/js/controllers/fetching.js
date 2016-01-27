154091b51f0ffc59bd9b3079ab7392352c211db8
Andres Moreno <amoreno@unu.edu>
2016-01-27 15:35:45 +0800

    Fetching switch does not require user to play save anymore

--- public/angular/js/controllers/fetching.js
+++ public/angular/js/controllers/fetching.js
@@ -3,8 +3,8 @@ angular.module('Aggie')
 .controller('FetchingController', [
   '$scope',
   'Fetching',
-  '$timeout',
-  function($scope, Fetching, $timeout) {
+  'FlashService',
+  function ($scope, Fetching, flash) {
 
     Fetching.get(function success(data) {
       $scope.on = data;
@@ -12,23 +12,17 @@ angular.module('Aggie')
 
     $scope.toggle = function (x) {
       $scope.on = x;
-    };
-
-    $scope.save = function () {
-      $scope.loading = true;
+      console.log('Toggling!');
       Fetching.set($scope.on, success, failure);
     };
 
-    function success (data) {
-      // a tiny delay just in case the response is
-      // so super-fast, user doesn't see any indication
-      $timeout(function () {
-        $scope.loading = false;
-      }, 300);
-    }
+    function success(data) {
+      flash.setNoticeNow('Fetching has been successfully switched.');
+    };
 
-    function failure (data) {
+    function failure(data) {
+      flash.setAlertNow('An error has occurred setting the fetching status');
       console.log('failure: ', data);
-    }
+    };
   }
 ]);
