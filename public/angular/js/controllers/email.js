4be37f8a4ebb9314d865d9ab7788b300aa087695
Andrés Moreno <andresfib@gmail.com>
2016-03-15 17:17:31 +0800

    4154: Mailer now reloads config before sending
    As email configuration can now change after startup, we fire event to reload transport or email address.
    
    Listeners bind to mailer object to properly used mailer's this.

--- public/angular/js/controllers/email.js
+++ public/angular/js/controllers/email.js
@@ -6,26 +6,29 @@ angular.module('Aggie')
   '$timeout',
   '$filter',
   'FlashService',
-//  'mediaSettingsOptions',
   '$modal',
   function($scope, Settings, $timeout, $filter, flash, $modal) {
 
-    $scope.mail = "";
+    $scope.mail = '';
     $scope.transport = {};
-    Settings.get('email', function success(data){
+    Settings.get('email', function success(data) {
       $scope.email = data.email.from;
       $scope.transport = angular.copy(data.email.transport);
-    }, failure)
+    }, failure);
 
     function success() {
       flash.setNoticeNow('The mail setting has been correctly saved');
-    };
+    }
 
     $scope.saveEmail = function() {
       Settings.set('email', { from: $scope.email, transport: $scope.transport }, success, failure);
     };
 
-    $scope.editTransport = function(){
+    $scope.editTransport = function() {
+      Settings.set('email', { from: $scope.email }, success, failure);
+    };
+
+    $scope.editTransport = function() {
       var modalInstance = $modal.open({
         controller: 'EmailSettingsModalInstanceController',
         templateUrl: 'templates/email_modal.html',
@@ -37,12 +40,12 @@ angular.module('Aggie')
       });
       modalInstance.result.then(function(method) {
         $scope.transport.method = method;
-      }); 
+      });
     };
 
     function failure(data) {
       flash.setAlertNow('An error has occurred saving the email setting');
       console.log('failure: ', data);
-    };
-  },
+    }
+  }
 ]);
