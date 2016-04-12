1bc8e892072b87c90d7559645032d9652cfa9355
Andrés Moreno <andresfib@gmail.com>
2016-04-12 23:33:01 +0800

    4247: Improved layout of settings page

--- public/angular/js/controllers/media/settings.js
+++ public/angular/js/controllers/media/settings.js
@@ -22,32 +22,21 @@ angular.module('Aggie')
 
     $scope.inHttp = $window.location.protocol === 'http:';
 
-    $scope.toggle = function(mediaName, value) {
-      var mediaSettings = $scope.media[mediaName];
-      var setting = {};
-
-      setting.on = value;
-
-      if (value && mediaSettings.configured || !value) {
-        Settings.set(mediaName, setting, success(mediaName, setting, value), failure);
-      } else {
-        var modalInstance = modal.create(mediaName, mediaSettings);
-      };
-    };
-
     function getSetting(name, index, mediaItems) {
       Settings.get(name, function success(data) {
         $scope.media[data.setting] = angular.copy(data[data.setting]);
       }, failure);
 
-    };
+    }
+
     function success(mediaName, setting, value) {
       $scope.media[mediaName].on = value;
-    };
+    }
 
     function failure(data) {
       flash.setAlertNow('An error has occurred setting the media status');
       console.log('failure: ', data);
-    };
-  },
+    }
+
+  }
 ]);
