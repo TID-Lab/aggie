64e84e125c0a3e161b633a448d41d7305d7fdda7
pgarrison <philipgarrison@cmu.edu>
2016-05-10 16:36:59 +0800

    Check that locale-debug.json is being used
    Also, a tweak to the eslint config for tests to allow
    `require('./init')`.

---
+++ test/public.angular.translations.test.js
@@ -0,0 +1,49 @@
+/*
+ * This test suite is for static translation files in the
+ * public/angular/translations/ directory. We test the files in this directory
+ * directly, rather than their api endpoint /translations/, for a variety of
+ * reasons.
+ *   (1) These files are served statically, which is very simple
+ *   (2) the code that serves these files is in lib/api.js which is monolithic
+ *       and difficult to unit test
+ *   (3) The filesystem is part of the internationalization feature which is
+ *       externally exposed, in the sense that it is exposed to users who may
+ *       want to add translations for a new language.
+ */
+'use strict';
+require('./init');
+var expect = require('chai').expect;
+var fs = require('graceful-fs');
+var path = require('path');
+var async = require('async');
+var _ = require('lodash');
+
+function getTranslations(callback) {
+   var dirname = 'public/angular/translations';
+   fs.readdir(dirname, function(err, filenames) {
+     if (err) return callback(err);
+     async.map(filenames, function(filename, mapCallback) {
+       fs.readFile(path.join(dirname, filename), function(err, json) {
+         if (err) return mapCallback(err);
+         mapCallback(null, [filename, JSON.parse(json)]);
+      });
+    }, function(err, allLanguages) {
+      if (err) return callback(err);
+      callback(null, _.object(allLanguages));
+    });
+  });
+}
+
+describe('Translations files', function() {
+  it('debug language should contain the union of the other languages',
+     function(done) {
+    getTranslations(function(err, allLanguages) {
+      if (err) return done(err);
+      var allKnownStrings = _.union(_.map(_.values(allLanguages), _.keys));
+      var debugFile = 'locale-debug.json';
+      expect(allLanguages).to.have.property(debugFile);
+      var debugTranslations = _.compact(allLanguages[debugFile]);
+      expect(debugTranslations).to.have.all.keys(allKnownStrings);
+    });
+  });
+});
