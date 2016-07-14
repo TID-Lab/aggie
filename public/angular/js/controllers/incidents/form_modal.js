8c52a63040b0ddf891e29c1a6bf1508ab36cd365
Andres Moreno <andresfib@gmail.com>
2016-07-14 16:44:42 +0800

    4599: Linking incidents to reports now redirect to the right place
    Improved implementation of linking multiple reports to an incident,
    and sorted issues related to the display of incident names in the
    reports tables, specially for newly created incidents.

--- public/angular/js/controllers/incidents/form_modal.js
+++ public/angular/js/controllers/incidents/form_modal.js
@@ -13,7 +13,7 @@ angular.module('Aggie')
   'FlashService',
   function($rootScope, $scope, $q, $state, $modal, $modalStack, $location, Incident, Report, flash) {
 
-    $scope.create = function (report) {
+    $scope.create = function(reports) {
       $modalStack.dismissAll();
       var modalInstance = $modal.open({
         controller: 'IncidentFormModalInstanceController',
@@ -22,29 +22,31 @@ angular.module('Aggie')
           users: ['User', function(User) {
             return User.query().$promise;
           }],
-          incident: function () {
+          incident: function() {
             return {
               veracity: null,
               closed: false
             };
           },
-          report: function () {
-            return report;
+          reports: function() {
+            return reports;
           }
         }
       });
 
       modalInstance.result.then(function(incident) {
         Incident.create(incident, function(inc) {
-          if (report) {
+          if (reports) {
             var batchMode = $location.url() === '/reports/batch';
-            report.read = true;
-            report._incident = inc._id;
-            Report.update({id: report._id}, report, function () {
+            var ids = reports.map(function(report) {
+              return report._id;
+            });
+
+            Report.linkToIncident({ids: ids, incident: inc._id}, function () {
               if (batchMode) {
                 $rootScope.$state.go('batch', {}, { reload: true });
               } else {
-                $rootScope.$state.go('reports', { r: report }, { reload: false });
+                $rootScope.$state.go('reports', { r: reports[0] }, { reload: false });
               }
             });
           } else {
@@ -69,8 +71,8 @@ angular.module('Aggie')
           incident: function() {
             return incident;
           },
-          report: function () {
-            return null;
+          reports: function() {
+            return [];
           }
         }
       });
@@ -104,14 +106,14 @@ angular.module('Aggie')
   'veracityOptions',
   'users',
   'incident',
-  'report',
-  function($scope, $modalInstance, incidentStatusOptions, veracityOptions, users, incident, report) {
+  'reports',
+  function($scope, $modalInstance, incidentStatusOptions, veracityOptions, users, incident, reports) {
     $scope.incident = angular.copy(incident);
     $scope.users = users;
     $scope.veracity = veracityOptions;
     $scope.showErrors = false;
-    $scope.report = report;
-    $scope.minimal = !!report;
+    $scope.reports = reports;
+    $scope.minimal = Boolean(reports);
     $scope.minimalLatLng = true;
 
     $scope.save = function(form) {
