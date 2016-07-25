838420372e2bbd57616963770b6c2a33f35fde4a
pgarrison <philipgarrison@cmu.edu>
2016-07-25 13:36:25 +0800

    Clean up
    `.migrate` does not belong in git (already ignored in .gitignore), and a
    style fix in the migration.

--- migrations/1468989776640-change-manager-role-to-monitor.js
+++ migrations/1468989776640-change-manager-role-to-monitor.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-console */
 'use strict';
 
 var User = require('../models/user');
 
 exports.up = function(next) {
   User.update({ role: 'manager' }, { role: 'monitor' }, { multi: true }, function(err, numAffected) {
-    if (err) console.log(err);
-    console.log('Number of managers changed to monitors' + numAffected);
+    if (err) next(err);
+    console.log('Number of managers changed to monitors: ' + numAffected);
     next();
   });
 };
