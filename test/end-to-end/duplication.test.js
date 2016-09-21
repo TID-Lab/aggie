a0e1e3ff812386d551ab37a6d9ba998479aaefe9
karthiksbhat <krtk.bhat@gmail.com>
2016-09-21 18:22:58 +0800

    Semi-complete tests: need to check correct generation of reports

--- test/end-to-end/duplication.test.js
+++ test/end-to-end/duplication.test.js
@@ -6,7 +6,7 @@ var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 var expect = chai.expect;
-var promise = protractor.promise;
+// var promise = protractor.promise;
 
 
 describe('test duplication of reports with different settings', function() {
@@ -23,14 +23,18 @@ describe('test duplication of reports with different settings', function() {
   keyword: 'test'
   };
 
+/*
   it('should add SMS Ghana source with keyword: test', function(done) {
     utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
+    utils.toggleSource('SMS GH', 'Off');
     done();
   });
+*/
   it('should listen with fetching:on and source:enabled', function(done) {
     utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
     utils.toggleFetching('On');
     utils.toggleSource('SMS GH', 'On');
+    // Sleep before this is sent.
     request('http://localhost:1111')
       .get('/smsghana')
       .query(reqParams)
@@ -41,17 +45,68 @@ describe('test duplication of reports with different settings', function() {
         }
       });
     browser.get(browser.baseUrl + 'reports');
+    // Check if there is only one. If not, throw error.
+    utils.toggleSource('SMS GH', 'Off');
+    utils.toggleFetching('Off');
     done();
   });
-/*
-  it('should not listen with fetching:on and source:disabled', function() {
+
+  it('should not listen with fetching:on and source:disabled', function(done) {
     utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
+    utils.toggleFetching('On');
+    utils.toggleSource('SMS GH', 'Off');
+    // Sleep before this is sent.
+    request('http://localhost:1111')
+      .get('/smsghana')
+      .query(reqParams)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+      });
+    browser.get(browser.baseUrl + 'reports');
+    // Check if there is any report. If there is, throw error.
+    utils.toggleFetching('Off');
+    done();
   });
-  it('should not listen with fetching:off and source:disabled', function() {
+
+  it('should not listening wtih fetching:off and source:enabled', function(done) {
     utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
+    utils.toggleFetching('Off');
+    utils.toggleSource('SMS GH', 'On');
+    // Sleep before this is sent.
+    request('http://localhost:1111')
+      .get('/smsghana')
+      .query(reqParams)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+      });
+    browser.get(browser.baseUrl + 'reports');
+    // Check if there is any report. If there is, throw error.
+    utils.toggleSource('SMS GH', 'Off');
+    done();
   });
-  it('should not listening wtih fetching:off and source:enabled', function() {
+
+  it('should not listen with fetching:off and source:disabled', function(done) {
     utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
+    utils.toggleFetching('Off');
+    utils.toggleSource('SMS GH', 'Off');
+    // Sleep before this is sent.
+    request('http://localhost:1111')
+      .get('/smsghana')
+      .query(reqParams)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+      });
+    browser.get(browser.baseUrl + 'reports');
+    // Check if there is any report. If there is, throw error.
+    done();
   });
-*/
 });
