9cb9edf1f977601af9a69874cb9aa4d4352a5a1b
karthiksbhat <krtk.bhat@gmail.com>
2016-09-21 16:59:41 +0800

    The having the request() function somehow does not empty the database Without it, reports remain at 0, and the code works. With it, reports increments by 2 each time (expected) but start from the previous value and not 0.

--- test/end-to-end/duplication.test.js
+++ test/end-to-end/duplication.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var utils = require('./e2e-tools');
+var request = require('supertest');
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
@@ -15,20 +16,42 @@ describe('test duplication of reports with different settings', function() {
   beforeEach(utils.resetDb);
   beforeEach(utils.initAdmin.bind({}, 'asdfasdf'));
   afterEach(utils.resetBrowser);
+  var reqParams = {
+  from: '9845098450',
+  fulltext: 'lorem ipsum dolor',
+  date: '2016-09-01',
+  keyword: 'test'
+  };
 
-  it('should add SMS Ghana source with keyword: test', function() {
+  it('should add SMS Ghana source with keyword: test', function(done) {
+    utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
+    done();
+  });
+  it('should listen with fetching:on and source:enabled', function(done) {
+    utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
+    utils.toggleFetching('On');
+    utils.toggleSource('SMS GH', 'On');
+    request('http://localhost:1111')
+      .get('/smsghana')
+      .query(reqParams)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+      });
+    browser.get(browser.baseUrl + 'reports');
+    done();
+  });
+/*
+  it('should not listen with fetching:on and source:disabled', function() {
     utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
   });
-  it('should listen with fetching:on and source:enabled', function() {
-
-  });
-  it('should not listen with fetching:on and source:disabled', function() {
-
-  });
   it('should not listen with fetching:off and source:disabled', function() {
-
+    utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
   });
   it('should not listening wtih fetching:off and source:enabled', function() {
-
+    utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
   });
+*/
 });
