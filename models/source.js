fa770475b3ffe50485b12d21d5b57cf7d2b46ba8
karthiksbhat <krtk.bhat@gmail.com>
2016-10-05 14:57:53 +0800

    Fetching is a little more trustworthy now Removed comments, fixed some old styling issues in source.js, fetching checks database each time it is toggled. This is a potential time-consumer. So, can be changed in the future.

--- models/source.js
+++ models/source.js
@@ -1,7 +1,8 @@
 // Represents a single source of data, e.g. a single Facebook page or RSS feed.
 // Only one Twitter source should exist due to rate limiting. Only one is needed since OR queries can be used.
 // Sources keep track of any errors or warnings that are encountered during fetching.
-// They also track how many of these errors have been 'read' so that the user can be notified if new errors have occurred since they last checked.
+// They also track how many of these errors have been 'read' so that the user can be notified if new errors
+// have occurred since they last checked.
 // The actual fetching is handled by the fetching module.
 
 var database = require('../lib/database');
@@ -12,12 +13,12 @@ require('../lib/error');
 
 var EVENTS_TO_RETURN = 50;
 
-var length_validator = validate({
+var lengthValidator = validate({
   validator: 'isLength',
   arguments: [0, 20]
 });
 
-var url_validator = validate({
+var urlValidator = validate({
   validator: 'isURL',
   passIfEmpty: true
 });
@@ -25,16 +26,16 @@ var url_validator = validate({
 var mediaValues = ['facebook', 'elmo', 'twitter', 'rss', 'dummy', 'smsgh'];
 
 var sourceSchema = new mongoose.Schema({
-  media: {type: String, enum: mediaValues},
-  nickname: {type: String, required: true, validate: length_validator},
+  media: { type: String, enum: mediaValues },
+  nickname: { type: String, required: true, validate: lengthValidator },
   resource_id: String,
-  url: {type: String, validate: url_validator},
+  url: { type: String, validate: urlValidator },
   keywords: String,
-  enabled: {type: Boolean, default: true},
-  events: {type: Array, default: []},
-  unreadErrorCount: {type: Number, default: 0},
+  enabled: { type: Boolean, default: true },
+  events: { type: Array, default: [] },
+  unreadErrorCount: { type: Number, default: 0 },
   lastReportDate: Date,
-  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false},
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false }
 });
 
 sourceSchema.pre('save', function(next) {
@@ -44,12 +45,14 @@ sourceSchema.pre('save', function(next) {
   if (!this.isNew && this.isModified('unreadErrorCount')) {
     this._sourceErrorCountUpdated = true;
   }
+
   if (!this.isNew && this.isModified('enabled')) {
     this._sourceStatusChanged = true;
   }
+
   // Only allow a single Twitter source
   if (this.isNew && this.media === 'twitter') {
-    Source.findOne({media: 'twitter'}, function(err, source) {
+    Source.findOne({ media: 'twitter' }, function(err, source) {
       if (source) return next(new Error.Validation('only_one_twitter_allowed'));
       else next();
     });
@@ -61,10 +64,9 @@ sourceSchema.pre('save', function(next) {
 sourceSchema.post('save', function() {
   if (this._sourceStatusChanged) {
     var event = (this.enabled) ? 'source:enable' : 'source:disable';
-    sourceSchema.emit(event, {_id: this._id.toString()});
-  }
-  else {
-    if (!this._silent) sourceSchema.emit('source:save', {_id: this._id.toString()});
+    sourceSchema.emit(event, { _id: this._id.toString() });
+  } else {
+    if (!this._silent) sourceSchema.emit('source:save', { _id: this._id.toString() });
   }
 
   if (this._sourceErrorCountUpdated) {
@@ -73,7 +75,7 @@ sourceSchema.post('save', function() {
 });
 
 sourceSchema.pre('remove', function(next) {
-  sourceSchema.emit('source:remove', {_id: this._id.toString()});
+  sourceSchema.emit('source:remove', { _id: this._id.toString() });
   next();
 });
 
@@ -89,7 +91,7 @@ sourceSchema.methods.disable = function() {
 
 // Log events in source
 sourceSchema.methods.logEvent = function(level, message, callback) {
-  this.events.push({datetime: new Date(), type: level, message: message});
+  this.events.push({ datetime: new Date(), type: level, message: message });
   if (level == 'error') this.disable();
   this.unreadErrorCount++;
   this._silent = true;
@@ -125,7 +127,7 @@ Source.resetUnreadErrorCount = function(_id, callback) {
 // Determine total number of errors
 Source.countAllErrors = function(callback) {
   var pipeline = [
-    {$group: {_id: null, unreadErrorCount: {$sum: "$unreadErrorCount"}}}
+    { $group: { _id: null, unreadErrorCount: { $sum: "$unreadErrorCount" } } }
   ];
   Source.aggregate(pipeline, function(err, total) {
     if (err) callback(err);
