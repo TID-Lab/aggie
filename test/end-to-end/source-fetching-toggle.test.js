6bb7e0d414845d842aadc5e9564ecd6f5198afa8
Philip Garrison <garrison.philip.m@gmail.com>
2016-10-17 16:39:20 +0800

    Abstract SMS GH fetching toggle tests
    All SMS GH fetching toggle tests are now basically one! Deleting code
    always feels good.

--- test/end-to-end/source-fetching-toggle.test.js
+++ test/end-to-end/source-fetching-toggle.test.js
@@ -30,6 +30,7 @@ describe('test duplication of reports with different settings', function() {
 
   var sendRequest = function() {
     var defer = promise.defer();
+    browser.sleep(500);
     browser.call(function() {
       request('http://localhost:1111')
         .get('/smsghana')
@@ -42,59 +43,31 @@ describe('test duplication of reports with different settings', function() {
     return defer.promise;
   };
 
-  it('should listen with fetching:on and source:enabled', function() {
-    utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
-    utils.toggleFetching('On');
-    utils.toggleSource('SMS GH', 'On');
-    browser.sleep(500);
-    browser.wait(sendRequest());
-    browser.get(browser.baseUrl + 'reports');
-    expect(utils.getReports().count()).to.eventually.equal(1);
-    utils.toggleSource('SMS GH', 'Off');
-    utils.toggleFetching('Off');
-    utils.deleteSource('SMS GH', 'hello');
-  });
+  var setAndExpect = function(fetchingOn, sourceOn, numExpect) {
+    return function() {
+      fetchingOn && utils.toggleFetching('On');
+      !sourceOn && utils.toggleSource('SMS GH', 'Off');
+      browser.wait(sendRequest());
+      expect(utils.getReports().count()).to.eventually.equal(numExpect);
+      fetchingOn && utils.toggleFetching('Off');
+      sourceOn && utils.toggleSource('SMS GH', 'Off');
+    };
+  };
 
-  it('should not listen with fetching:on and source:disabled', function() {
-    utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
-    utils.toggleFetching('On');
-    utils.toggleSource('SMS GH', 'Off');
-    browser.sleep(500);
-    browser.wait(sendRequest());
-    expect(utils.getReports().count()).to.eventually.equal(0);
-    utils.toggleFetching('Off');
-    utils.deleteSource('SMS GH', 'hello');
-  });
+  it('should listen with fetching:on and source:enabled',
+     setAndExpect(true, true, 1));
 
-  it('should not listening wtih fetching:off and source:enabled', function() {
-    utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
-    utils.toggleFetching('Off');
-    utils.toggleSource('SMS GH', 'On');
-    browser.sleep(500);
-    browser.wait(sendRequest());
-    expect(utils.getReports().count()).to.eventually.equal(0);
-    utils.toggleSource('SMS GH', 'Off');
-    utils.deleteSource('SMS GH', 'hello');
-  });
+  it('should not listen with fetching:on and source:disabled',
+     setAndExpect(true, false, 0));
 
-  it('should not listen with fetching:off and source:disabled', function() {
-    utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
-    utils.toggleFetching('Off');
-    utils.toggleSource('SMS GH', 'Off');
-    browser.sleep(500);
-    browser.wait(sendRequest());
-    expect(utils.getReports().count()).to.eventually.equal(0);
-    utils.deleteSource('SMS GH', 'hello');
-  });
+  it('should not listening wtih fetching:off and source:enabled',
+     setAndExpect(false, true, 0));
+
+  it('should not listen with fetching:off and source:disabled',
+     setAndExpect(false, false, 0));
 
   it('should not listen with fetching toggled from on to off and source:disabled', function() {
-    utils.addSource('SMS GH', { nickname: 'hello', keywords: 'test' });
     utils.toggleFetching('On');
-    utils.toggleFetching('Off');
-    utils.toggleSource('SMS GH', 'Off');
-    browser.sleep(500);
-    browser.wait(sendRequest());
-    expect(utils.getReports().count()).to.eventually.equal(0);
-    utils.deleteSource('SMS GH', 'hello');
+    setAndExpect(false, false, 0)();
   });
 });
