c85bceb3653617f96de43ff91e8595aac29c7c8a
Philip Garrison <philipgarrison@unu.edu>
2016-11-02 15:48:10 +0800

    Turn tags into an array on the backend.
    We can't pass an array of one element in the query string without any
    extra processing, so just do the work on the server.

--- public/angular/js/services/tags.js
+++ public/angular/js/services/tags.js
@@ -5,14 +5,6 @@ angular.module('Aggie')
   return {
     tagsToString: function(tags) {
       return tags.join(', ');
-    },
-    stringToTags: function(tags) {
-      if (typeof tags === 'string') {
-        return tags.split(',').map(function(tag) {
-          return tag.trim();
-        });
-      }
-      return tags;
     }
   };
 });
