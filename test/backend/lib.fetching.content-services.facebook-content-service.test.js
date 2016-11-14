6649d8e59559107f0f0477e4d6ad7ff25c4adb88
Philip Garrison <philipgarrison@unu.edu>
2016-11-14 13:20:19 +0800

    Facebook metadata being stored

--- test/backend/lib.fetching.content-services.facebook-content-service.test.js
+++ test/backend/lib.fetching.content-services.facebook-content-service.test.js
@@ -47,10 +47,12 @@ describe('Facebook content service', function() {
   it('should fetch mock content from Facebook', function(done) {
     var service = stubWithFixture('facebook-1.json');
     var fetched = 0;
-
+    console.log("before error");
     service.once('error', function(err) { done(err); });
-
+    console.log("before report");
     service.on('report', function(reportData) {
+      console.log('entered this');
+      console.log(reportData);
       expect(reportData).to.have.property('fetchedAt');
       expect(reportData).to.have.property('authoredAt');
       expect(reportData).to.have.property('content');
@@ -74,11 +76,12 @@ describe('Facebook content service', function() {
         return done(new Error('Unexpected report'));
       }
     });
-
+    console.log('At the end of this');
     // Give enough time for extra report to appear.
-    setTimeout(function() { if (fetched == 3) done(); }, 100);
-
+    setTimeout(function() { console.log('fetched'); console.log(fetched); if (fetched == 3) done(); }, 100);
+    console.log('before service.fetch');
     service.fetch({ maxCount: 50 }, function() {});
+    console.log('after service.fetch');
   });
 
   it('should get new comments from old posts, excluding already added posts', function(done) {
