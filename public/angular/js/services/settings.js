cf62e051ae6f89228d7b1859ea49590965d33902
Andrés Moreno <andresfib@gmail.com>
2017-11-14 09:06:20 +0100

    5543: We dont need to request the public API for gPlaces setting

--- public/angular/js/services/settings.js
+++ public/angular/js/services/settings.js
@@ -6,17 +6,10 @@ angular.module('Aggie')
     test: { method: 'POST', ignoreLoadingBar: true }
   });
 
-  var SettingsPublic = $resource('/api/v1/public/settings/:type/:item/:action', {}, {});
-
   return {
     get: function(item, success, failure) {
-      var resource;
-      if (item === 'gplaces') {
-        resource = SettingsPublic;
-      } else {
-        resource = Settings;
-      }
-      return resource.get({ item: item }, function(data) {
+
+      return Settings.get({ item: item }, function(data) {
         success(data);
       }, failure);
     },
