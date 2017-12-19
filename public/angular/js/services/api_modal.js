35fb70857213c38588b016597463bca893e3d7bb
Andrés Moreno <andresfib@gmail.com>
2017-12-19 17:12:17 +0200

    5543: Integrate API and widget settings modal in one controller
    We move the modal creation from a service to a controller

--- public/angular/js/services/api_modal.js
+++ public/angular/js/services/api_modal.js
@@ -1,21 +1,21 @@
-angular.module('Aggie')
+// angular.module('Aggie')
 
-.factory('APISettingsModal', function($modal) {
+// .factory('APISettingsModal', function($modal) {
 
-  return {
-    create: function(api, settings) {
-      var modalInstance = $modal.open({
-        controller: 'APISettingsModalInstanceController',
-        templateUrl: 'templates/api-settings-modal.html',
-        resolve: {
-          api: function() {
-            return api;
-          },
-          settingsValues: function() {
-            return settings;
-          }
-        }
-      });
-    }
-  };
-});
+//   return {
+//     create: function(api, settings) {
+//       var modalInstance = $modal.open({
+//         controller: 'APISettingsModalInstanceController',
+//         templateUrl: 'templates/api-settings-modal.html',
+//         resolve: {
+//           api: function() {
+//             return api;
+//           },
+//           settingsValues: function() {
+//             return settings;
+//           }
+//         }
+//       });
+//     }
+//   };
+// });
