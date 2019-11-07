6efaa2ee7d322bb156986f9ec91ac082672c5e30
Daniel Nkemelu <daniel.nkemelu@gmail.com>
2019-11-07 13:31:04 -0500

    Updated crowdtangle test service

--- test/backend/lib.fetching.content-service.crowd-tangle-content-service.test.js
+++ test/backend/lib.fetching.content-service.crowd-tangle-content-service.test.js
@@ -56,32 +56,24 @@ describe('CrowdTangle content service', function() {
       expect(reportData).to.have.property('metadata');
       switch (++fetched) {
       case 1:
-        expect(reportData.content).to.contain('The ACC Champion');
-        expect(reportData.author).to.equal('Georgia Tech');
+        expect(reportData.content).to.contain('Happy Birthday');
+        expect(reportData.author).to.equal('Philadelphia Phillies');
         expect(reportData.url).to.contain('https');
-        expect(reportData.metadata.likeCount).to.equal(41);
-        expect(reportData.metadata.reactionCount).to.equal(42);
-        expect(reportData.metadata.place).to.equal('Area 51');
+        expect(reportData.metadata.type).to.equal('photo');
         break;
       case 2:
-        expect(reportData.content).to.contain('Bioengineers at');
-        expect(reportData.author).to.equal('Georgia Tech');
-        expect(reportData.metadata.likeCount).to.equal(43);
-        expect(reportData.metadata.reactionCount).to.equal(44);
-        expect(reportData.metadata.place).to.equal('Area 52');
+        expect(reportData.content).to.contain('innovative ways to sit around');
+        expect(reportData.author).to.equal('Taylor Swift');
+        expect(reportData.metadata.platform).to.equal('Facebook');
+        expect(reportData.metadata.type).to.equal('native_video');
         break;
       case 3:
-        expect(reportData.content).to.contain('Amazing');
-        expect(reportData.author).to.equal('Test User 1');
-        expect(reportData.metadata.likeCount).to.equal(0);
-        break;
-      case 4:
         return done(new Error('Unexpected report'));
       }
     });
 
     // Give enough time for extra report to appear.
-    setTimeout(function() { if (fetched == 3) done(); }, 100);
+    setTimeout(function() { if (fetched == 2) done(); }, 100);
 
     service.fetch({ maxCount: 50 }, function() {});
   });
