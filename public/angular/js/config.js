3a12736a85e5e0df0fcb425ed168d30cfa85ee7a
Daniel Nkemelu <daniel.nkemelu@gmail.com>
2020-01-15 14:10:39 -0500

    Added crowdtangle to media list

--- public/angular/js/config.js
+++ public/angular/js/config.js
@@ -1,6 +1,6 @@
 angular.module('Aggie')
 
-.value('mediaOptions', ['twitter', 'facebook', 'rss', 'elmo', 'smsgh', 'whatsapp'])
+.value('mediaOptions', ['twitter', 'crowdtangle', 'facebook', 'rss', 'elmo', 'smsgh', 'whatsapp']) // 'facebook'
 
 .value('apiSettingsOptions', ['twitter', 'facebook', 'elmo', 'gplaces'])
 
