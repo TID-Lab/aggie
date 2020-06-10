9a164138fade49638c821c7fc5ac745817a7964f
Max Karpawich <maxkarpawich@gmail.com>
2020-06-10 17:03:35 -0400

    feat: add Report removal button to Incident "show" page

--- public/angular/js/services/report.js
+++ public/angular/js/services/report.js
@@ -9,6 +9,7 @@ angular.module('Aggie')
     'update': { method: 'PUT' },
     'toggleRead': { method: 'PATCH', url: '/api/v1/report/_read', isArray: false },
     'toggleFlagged': { method: 'PATCH', url: '/api/v1/report/_flag', isArray: false },
-    'linkToIncident': { method: 'PATCH', url: '/api/v1/report/_link', isArray: false }
+    'linkToIncident': { method: 'PATCH', url: '/api/v1/report/_link', isArray: false },
+    'unlinkFromIncident': { method: 'PATCH', url: '/api/v1/report/_unlink', isArray: false }
   });
 });
