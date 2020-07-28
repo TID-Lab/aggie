5e85e67a145bcd4d406b0a4cd218f0bf3802c640
Matthew Lim <mlim45@gatech.edu>
2020-07-28 16:57:13 -0400

    Revert "Revert "test: SMTC model""
    This reverts commit 5a338b3259f5b119ab08e31d63832a21a8e04cbf.

---
+++ test/backend/models.tag.test.js
@@ -0,0 +1,41 @@
+var utils = require('./init');
+var chai = require('chai');
+var should = chai.should();
+var SMTCTag = require('../../models/tag');
+
+describe('SMTCTag attributes', function() {
+    before(function(done) {
+        tag = new SMTCTag({
+            name: 'disinformation'
+        });
+        done();
+    });
+
+    it('name should be a string', function() {
+        tag.name.should.be.a('string');
+    });
+
+    it('should save a tag', function(done) {
+        tag.save(done);
+    });
+
+    it('should find our newly created tag', function(done) {
+        SMTCTag.findOne({ name: tag.name }, function(err, tag) {
+            should.exist(tag);
+            tag.name.should.equal('disinformation');
+            done();
+        })
+    });
+
+    it('should not allow tags with duplicate name', function(done) {
+        var dupe = new SMTCTag({ name: 'disinformation' });
+        dupe.save(function(err) {
+            err.status.should.equal(422);
+            err.message.should.equal('name_not_unique');
+            done();
+        });
+    });
+
+    after(utils.wipeModels([SMTCTag]));
+    after(utils.expectModelsEmpty);
+})
