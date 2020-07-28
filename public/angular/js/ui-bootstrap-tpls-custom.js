e46e91eac2b102c726f1172c1e0f665ad91e947a
Matthew Lim <mlim45@gatech.edu>
2020-07-28 01:12:30 -0400

    Fixed Layout Problems

--- public/angular/js/ui-bootstrap-tpls-custom.js
+++ public/angular/js/ui-bootstrap-tpls-custom.js
@@ -4062,7 +4062,7 @@ angular.module("template/modal/backdrop.html", []).run(["$templateCache", functi
 angular.module("template/modal/window.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/modal/window.html",
     "<div tabindex=\"-1\" role=\"dialog\" class=\"modal fade\" ng-class=\"{show: animate}\" aria-modal=\"true\" ng-style=\"{'z-index': 1050 + index*10, display: 'block', 'padding-right': '17px'}\" ng-click=\"close($event)\">\n" +
-    "    <div class=\"modal-dialog\" ng-class=\"{'modal-sm': size == 'sm', 'modal-lg': size == 'lg'}\"><div class=\"modal-content\" modal-transclude></div></div>\n" +
+    "    <div class=\"modal-dialog\" ng-class=\"{'modal-sm': size == 'sm', 'modal-lg': size == 'lg', 'modal-xl': size == 'xl'}\"><div class=\"modal-content\" modal-transclude></div></div>\n" +
     "</div>");
 }]);
 
