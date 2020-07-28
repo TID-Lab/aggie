63a4a3e5fa4d5221b01bef0b6cfa597bed00b9a9
Matthew Lim <mlim45@gatech.edu>
2020-07-28 16:57:21 -0400

    Revert "Revert "test: add SMTC model to general db setup & teardown routines""
    This reverts commit e752a156a4c57e9325b8b325dc4faa776fd31907.

--- test/database-tools.js
+++ test/database-tools.js
@@ -7,6 +7,7 @@ var Report = require('../models/report');
 var Source = require('../models/source');
 var Trend = require('../models/trend');
 var Incident = require('../models/incident');
+var SMTCTag = require('../models/tag');
 
 exports.initDb = function(callback) {
   async.series([
@@ -45,7 +46,7 @@ exports.wipeModels = function(models) {
 
 exports.resetDb = function(callback) {
   async.series([
-    exports.wipeModels([Report, Source, Trend, Incident, User]),
+    exports.wipeModels([Report, Source, Trend, Incident, User, SMTCTag]),
     function(next) {
       // Create admin user for testing
       User.create({
