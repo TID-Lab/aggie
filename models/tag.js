7082a58146610e56f5e72cbd6586e4f1f20dafd3
Matthew Lim <mlim45@gatech.edu>
2020-07-28 16:57:19 -0400

    Revert "Revert "feat: add SMTCTag model for report-level tagging""
    This reverts commit 2145c3b05be4117d8b1c1f9f8d63a0812ace9525.

---
+++ models/tag.js
@@ -0,0 +1,52 @@
+// Represents a categorization of a report by an SMTC monitor
+
+var database = require('../lib/database');
+var mongoose = database.mongoose;
+var logger = require('../lib/logger');
+var _ = require('underscore');
+
+var tagSchema = new mongoose.Schema({
+    name: {type: String, required: true, unique: true }
+});
+
+tagSchema.pre('save', function(next) {
+    var tag = this;
+
+    if(!tag.name) return next(new Error.Validation("name_required"));
+
+    // Check for uniqueness
+    SMTCTag.checkUnique(tag, function(unique, err) {
+        if (!unique) return next(new Error.Validation(err));
+        else next();
+    });
+
+});
+
+tagSchema.post('save', function() {
+    schema.emit('tag:new', {
+        _id: this._id.toString(),
+        name: this.name
+    });
+});
+
+tagSchema.post('remove', function() {
+    tagSchema.emit('tag:removed', this._id);
+});
+
+var SMTCTag = mongoose.model('SMTCTag', tagSchema);
+
+SMTCTag.checkUnique = function(tag, callback) {
+    var query = { $and: [
+        { _id: { $ne: tag._id } },
+        { name: tag.name }
+    ]};
+    SMTCTag.count(query, function(err, count) {
+        if (err) {
+            logger.warning(err);
+        }
+        if (count) callback(false, _.keys(_.last(query.$and))[0] + '_not_unique');
+        else callback(true);
+    })
+}
+
+module.exports = SMTCTag;
