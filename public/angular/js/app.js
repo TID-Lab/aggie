d90b7665c3af1e7eaa38f1ea0002b25ea1ea60b8
Matthew Lim <mlim45@gatech.edu>
2020-07-29 20:17:20 -0400

    Added tagify stylesheets and removed select2 libraries
    We don't actually use select2 for anything so I have removed the library.

--- public/angular/js/app.js
+++ public/angular/js/app.js
@@ -5,8 +5,6 @@ require('./ui-bootstrap-0.12.0');
 require('./ui-bootstrap-tpls-custom');
 require('./angular-resource');
 require('./jquery.sparkline');
-require('../vendor/select2/select2');
-require('../vendor/select2/ui-select2');
 require('./loading-bar.min');
 require('angular-translate');
 require('angular-translate-loader-static-files');
@@ -14,7 +12,7 @@ require('angular-translate-handler-log');
 require('angular-cookies');
 
 angular.module('Aggie', ['ui.router', 'ui.bootstrap', 'ngResource',
-  'pascalprecht.translate', 'ui.select2', 'ngSanitize', 'ngAutocomplete',
+  'pascalprecht.translate', 'ngSanitize', 'ngAutocomplete',
   'angular-loading-bar', 'ngCookies'])
 
 .config(['$urlRouterProvider', '$locationProvider',
