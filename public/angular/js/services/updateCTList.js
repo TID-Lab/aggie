940aaba3dd5f758d0634f195340577df2cbf8a33
Min Htat Kyaw <minhtatdx00@gmail.com>
2020-07-31 14:40:14 -0700

    Make the directory to save file generalizable

--- public/angular/js/services/updateCTList.js
+++ public/angular/js/services/updateCTList.js
@@ -1,7 +1,7 @@
 angular.module('Aggie')
 
 .factory('UpdateCTList', function($resource) {
-  return $resource('/api/v1/settings/updateCTList', null, {
+  return $resource('/api/v1/settings/updateCTList/:directory', {directory: '../../config/crowdtangle_list.json'}, {
     'update': { method: 'PUT' },
   });
 });
