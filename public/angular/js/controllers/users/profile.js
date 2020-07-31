7ad50f66b204857556390c549b64036b5ef6a94b
Matthew Lim <mlim45@gatech.edu>
2020-07-30 23:39:42 -0400

    Added Statsbar to User Pages

--- public/angular/js/controllers/users/profile.js
+++ public/angular/js/controllers/users/profile.js
@@ -8,20 +8,28 @@ angular.module('Aggie')
   '$stateParams',
   'users',
   'User',
-  function($scope, $rootScope, stateParams, users, User) {
+  'Socket',
+  function($scope, $rootScope, stateParams, users, User, Socket) {
     $scope.users = users;
-
-    function init() {
+    var init = function() {
+      Socket.on('stats', updateStats);
+      Socket.join('stats');
       if ($rootScope.currentUser) {
         $scope.currentUser = $rootScope.currentUser;
         $scope.user = _.find($scope.users, function(u) {
           return u.username === stateParams.userName;
         });
       }
+
     }
-
+    var updateStats = function(stats) {
+      $scope.stats = stats;
+    };
     $rootScope.$watch('currentUser', init);
-
+    $scope.$on('$destroy', function() {
+      Socket.leave('stats');
+      Socket.removeAllListeners('stats');
+    });
     init();
   }
 ]);
