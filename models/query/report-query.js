c2943a7f1ce6107c30fc77566305a45f4f3d1ef1
RoboticWater <jakebritti@gmail.com>
2020-09-24 14:13:47 -0400

    fixed ct list query to work with new report format

--- models/query/report-query.js
+++ models/query/report-query.js
@@ -6,7 +6,7 @@ var Query = require('../query');
 var util = require('util');
 var _ = require('lodash');
 
-function ReportQuery(options, list_pairs) {
+function ReportQuery(options) {
   options = options || {};
   this.keywords = options.keywords;
 
@@ -24,7 +24,6 @@ function ReportQuery(options, list_pairs) {
   this.event = 'reports';
   this.tags = options.tags;
   this.list = options.list;
-  this.list_pairs = list_pairs
 }
 
 _.extend(ReportQuery, Query);
@@ -55,17 +54,7 @@ ReportQuery.prototype.toMongooseFilter = function() {
   if (this.author)    filter.author = { $regex: this.author, $options: 'i'}
   if (this.keywords)  filter.content = { $regex: this.keywords,  $options: 'i' }
   if (this.tags)      filter.tags = { $in: this.tags }
-  if (this.list) {
-    var listTags = Object.entries(this.list_pairs).reduce(function(acc, cur) {
-      return cur[1].includes(this.list) ? acc.concat(cur[0]) : acc;
-    }.bind(this), [])
-    console.log(listTags);
-    if (filter.tags) {
-      filter.tags.$in = Array.from(new Set(filter.tags.$in.concat(tags)));
-    } else {
-      filter.tags = { $in: listTags };
-    }
-  }
+  if (this.list)      filter["metadata.ct_tag"] = {$in: [this.list] }
   return filter;
 };
 
