5b1ad393d8ea000985847cf71e2ef5d3ababcbf3
Arpit Mathur <arpitmathur19@gmail.com>
2020-10-13 11:43:52 -0400

    D3 with Angular

--- public/angular/js/controllers/analysis.js
+++ public/angular/js/controllers/analysis.js
@@ -18,6 +18,45 @@ angular.module('Aggie')
       Socket.removeAllListeners('stats');
     });
 
+    $scope.initiateDashboard = function() {
+      $scope.svgBar = d3.select('figure#aggie-viz')
+      .append('svg').attr('width', '100%').attr('viewBox', '0 0 1400 800');
+    }
+
+    $scope.processData = function(data) {
+
+      let actualStatistics = [];
+      let expectedStatistics = [];
+
+      for (let property in data[0].metadata.actualStatistics) {
+        actualStatistics.push({
+          type: property,
+          count: 0
+        });
+    
+        expectedStatistics.push({
+          type: property,
+          count: 0
+        });
+      }
+    
+      for (let post of data) {
+        for (let prop in post.metadata.actualStatistics) {
+          let actReaction = actualStatistics.find(reaction => reaction.type == prop);
+          let expReaction = expectedStatistics.find(reaction => reaction.type == prop);
+          actReaction.count += post.metadata.actualStatistics[prop];
+          expReaction.count += post.metadata.expectedStatistics[prop];
+        }
+      }
+
+      $scope.actualStatistics = actualStatistics;
+      $scope.expectedStatistics = expectedStatistics;
+
+      console.log($scope.actualStatistics);
+    }
+
     init();
+    $scope.processData($scope.data);
+    $scope.initiateDashboard();
   }
 ]);
