fb1f1ab22d226a5dce9cf0c952536d360ca31c80
RoboticWater <jakebritti@gmail.com>
2020-10-15 19:44:30 -0400

    no data handler + fixed date range

--- public/angular/js/services/visualization.js
+++ public/angular/js/services/visualization.js
@@ -3,8 +3,9 @@ angular.module('Aggie')
 .factory('Visualization', function($resource) {
 	var endDate = new Date();
 	var startDate = new Date();
-	startDate.setTime(startDate.getTime() - 5 * (24 * 60 * 60 * 1000));
-	return $resource(
+	startDate.setTime(startDate.getTime() - 2 * (24 * 60 * 60 * 1000));
+	console.log("/api/v1/viz/?before=" + endDate.toISOString() + "&after=" + startDate.toISOString());
+  return $resource(
 		"/api/v1/viz/?before=" + endDate.toISOString() + "&after=" + startDate.toISOString(),
 		null,
 		{
