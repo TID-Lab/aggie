c4e175912c31d9ee1482f88ff0dec940a01c986c
Ubuntu <azureuser@MyanmarAggie.idltczic1e2edmzqycc43nqdef.bx.internal.cloudapp.net>
2020-10-28 16:17:03 +0000

    refactored  to avoid undefined in the string

--- models/query/report-query.js
+++ models/query/report-query.js
@@ -53,7 +53,7 @@ ReportQuery.prototype.toMongooseFilter = function() {
   if (this.after)     filter.storedAt = Object.assign({}, filter.storedAt, { $gte: this.after });
   //Two step search for content/author. First search for any terms in content or author using the indexed $text search.
   //Second step is to match exact phrase using regex in the returned superset of the documents from first step.
-  if (this.author || this.keywords) filter.$and = [{$text: { $search: this.author +" "+ this.keywords}}];
+  if (this.author || this.keywords) filter.$and = [{$text: { $search: `${this.author || ""} ${this.keywords || ""}` }}];
   if (this.author)    filter.$and.push({"author": {$regex: this.author, $options: 'i'}});
   if (this.keywords)  filter.$and.push({"content": {$regex: this.keywords, $options: 'i'}});
   if (this.tags)      filter.smtcTags = { $all: this.tags }
