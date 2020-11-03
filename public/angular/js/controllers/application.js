eb2f99212a6e39a6eb98a2c6bdc3eabbfe1be700
Arpit Mathur <arpitmathur19@gmail.com>
2020-11-03 16:16:43 -0500

    Configuration Settings for Matomo

--- public/angular/js/controllers/application.js
+++ public/angular/js/controllers/application.js
@@ -3,7 +3,27 @@ angular.module('Aggie')
 .controller('ApplicationController', [
   '$scope',
   'FlashService',
-  function($scope, flash) {
+  'Settings',
+  function($scope, flash, settings) {
     $scope.flash = flash;
+
+    settings.get('matomo', function(data) {
+      data.$promise.then(function(e) {
+        if (e.matomo.send_data) {
+          var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(["setCookieDomain", "*.aggiebeta.cartercenter.org"]);
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u=e.matomo.dashboard_url;
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', e.matomo.id_site]);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/cartercenter.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+        }
+      });
+    });    
   }
 ]);
