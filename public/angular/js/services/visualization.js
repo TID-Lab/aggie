8b9b057dff0e3e9bbd3ab607af080c06c6d275c0
RoboticWater <jakebritti@gmail.com>
2020-11-05 17:23:40 -0500

    tabs to spaces

--- public/angular/js/services/visualization.js
+++ public/angular/js/services/visualization.js
@@ -3,9 +3,9 @@ angular.module('Aggie')
     var endDate = new Date();
     var startDate = new Date();
     startDate.setTime(startDate.getTime() - 2 * (24 * 60 * 60 * 1000));
-		// var endDate = new Date("2020-09-21T09:00:59.000Z");
-		// var startDate = new Date("2020-09-21T09:00:59.000Z");
-		// startDate.setTime(startDate.getTime() - 2 * (24 * 60 * 60 * 1000));
+    // var endDate = new Date("2020-09-21T09:00:59.000Z");
+    // var startDate = new Date("2020-09-21T09:00:59.000Z");
+    // startDate.setTime(startDate.getTime() - 2 * (24 * 60 * 60 * 1000));
     // console.log("/api/v1/viz/?before=" + endDate.toISOString() + "&after=" + startDate.toISOString());
     return $resource(
       "/api/v1/viz/?before=" + endDate.toISOString() + "&after=" + startDate.toISOString(),
