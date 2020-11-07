a19ec73811d3646e548c6de0b5954e52c9f5bb50
Arpit Mathur <arpitmathur19@gmail.com>
2020-11-06 20:03:36 -0500

    Click tracking (#421)

--- public/angular/js/controllers/application.js
+++ public/angular/js/controllers/application.js
@@ -18,18 +18,35 @@ angular.module('Aggie')
         return;
       }
 
+      // Pageview Tracking
       var _paq = window._paq = window._paq || [];
       /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
       _paq.push(["setCookieDomain", data.matomo.cookie_domain]);
-      _paq.push(['trackPageView']);
       _paq.push(['enableLinkTracking']);
       (function() {
         var u='https://' + data.matomo.dashboard_name + '.matomo.cloud/';
         _paq.push(['setTrackerUrl', u+'matomo.php']);
         _paq.push(['setSiteId', data.matomo.site_id]);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/' + data.matomo.dashboard_name + '.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+        var d=document, 
+          g=d.createElement('script'), 
+          s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; 
+        g.async=true; 
+        g.src='//cdn.matomo.cloud/' + data.matomo.dashboard_name + '.matomo.cloud/matomo.js'; 
+        s.parentNode.insertBefore(g,s);
       })();
+
+      // Tag Management
+      var _mtm = window._mtm = window._mtm || [];
+      _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+      var d=document, 
+        g=d.createElement('script'), 
+        s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; 
+      g.async=true; 
+      g.src='https://cdn.matomo.cloud/' + data.matomo.dashboard_name +'.matomo.cloud/container_' + data.matomo.container_id  + '.js'; 
+      s.parentNode.insertBefore(g,s);
+
     }, function failure(e) {
       console.error("Couldn't set up Matomo:", e);
     });
