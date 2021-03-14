6d34951655839f5b2101efa051e8135f2c14dab8
Cuong Nguyen <cnguyen319@gatech.edu>
2021-03-14 12:34:27 -0400

    Remove flags backend and frontend (#448)
    * Minor changes
    
    * Created API endpoints for escalation and notes
    
    * "Removed" all referenes to flag in the backend
    
    * Update table.html
    
    * Add files via upload
    
    * Remove all references to flag in backend
    
    * Add files via upload
    
    * Update report.js
    
    * Update report-query.js
    
    * Attempt to merge flag removals #1
    
    * Attempt merge #1
    
    * Attempting to debug merge issue
    
    * Remove accidental crowdtangle data
    
    * Found a few extra.
    
    Co-authored-by: Ameya Jadhav <jadhavameyak@gmail.com>
    Co-authored-by: Matthew Lim <limmat@gatech.edu>

--- public/angular/js/services/report.js
+++ public/angular/js/services/report.js
@@ -9,7 +9,6 @@ angular.module('Aggie')
     'save': { method: 'PUT' },
     'update': { method: 'PUT' },
     'toggleRead': { method: 'PATCH', url: '/api/v1/report/_read', isArray: false },
-    'toggleFlagged': { method: 'PATCH', url: '/api/v1/report/_flag', isArray: false },
     'addSMTCTag': { method: 'PATCH', url: '/api/v1/report/_tag', isArray: false },
     'removeSMTCTag': { method: 'PATCH', url: '/api/v1/report/_untag', isArray: false },
     'clearSMTCTags': { method: 'PATCH', url: '/api/v1/report/_clearTags', isArray: false},
