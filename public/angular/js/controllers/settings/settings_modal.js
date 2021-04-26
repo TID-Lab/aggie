99541fdd4e3931979b5cffbebd04c3ff24f9f0cd
Ciabhan <ciabhanconnelly@gmail.com>
2021-04-26 03:25:40 -0400

    Improvements
    Let's see if this works on other devices. Working better on mine over large numbers of reports... though mongodump is still better if you really need to do a lot...

--- public/angular/js/controllers/settings/settings_modal.js
+++ public/angular/js/controllers/settings/settings_modal.js
@@ -55,46 +55,50 @@ angular.module('Aggie')
     };
 
     $scope.download = function(before, after) {
-    var url = "/api/v1/viz";
+    var url = "/api/v1/csv";
     var afterDate = "";
-
     if(after === undefined){
       afterDate = "2000-01-01T00:00:00.000Z"
     }
     else{
-      afterDate = after + ":00.277Z";
+      console.log(after)
+      afterDate = after + ":00.000Z";
     }
     if(before === undefined){
       var today = new Date();
       var bString = today.getFullYear() + "-" + String(today.getMonth() + 1).padStart(2, '0') +"-"+ String(today.getDate()).padStart(2, '0') + "T00:00:00.000Z";
+      console.log(bString)
       url = url.concat("/?before=" + bString + "&after=" + afterDate);
     }
     else{
-      url = url.concat("/?before=" + before + ":00.277Z" + "&after=" + afterDate);
+    console.log(before)
+      url = url.concat("/?before=" + before + ":00.000Z" + "&after=" + afterDate);
     }
-
-    //gets reports from viz code
     $resource(url).get().$promise.then(function(res){ //success function
-      var toWrite = "author \t authoredAt \t content \t fetchedAt \t flagged \t read \t tags \t url \t media";
-      for(i=0; i < res.reports.length; i++) {
+       
+      var csv = "author \t authoredAt \t content \t fetchedAt \t flagged \t read \t tags \t url \t media";
+       
+      for(var i=0; i < res.reports.length; i++) {
         //for each of the given rows, remove line breaks/tabs to correcly format output
         //output is a tsv, since commas frequently show up in plain
-        var newString = "\n" + res.reports[i].author.replace(/(?:\r\n|\r|\n)/g, ' ') + "\t" +
-                                res.reports[i].authoredAt.replace(/(?:\r\n|\r|\n)/g,' ') + "\t"+
-                                res.reports[i].content.replace(/(?:\r\n|\r|\n)/g,' ') + "\t"+
-                                res.reports[i].fetchedAt.replace(/(?:\r\n|\r|\n)/g,' ') + "\t"+
+        var newString = "\n" + res.reports[i].author.replace(/(?:\r\n|\r|\n)/g, ' ') + "\t"+
+                                res.reports[i].authoredAt.replace(/(?:\r\n|\r|\n)/g, ' ') + "\t"+
+                                res.reports[i].content.replace(/(?:\r\n|\r|\n)/g, ' ') + "\t"+
+                                res.reports[i].fetchedAt.replace(/(?:\r\n|\r|\n)/g, ' ') + "\t"+
                                 res.reports[i].flagged +"\t"+
                                 res.reports[i].read +"\t"+
-                                res.reports[i].tags[0].replace(/(?:\r\n|\r|\n)/g,' ') + "\t"+
-                                res.reports[i].url.replace('\t','') +"\t"+
-                                res.reports[i]._media[0].replace(/(?:\r\n|\r|\n)/g,' ');
-        toWrite = toWrite.concat(newString);         
-      }
+                                res.reports[i].tags +"\t"+
+                               res.reports[i].url.replace('\t','') +"\t"+
+                                res.reports[i]._media[0].replace(/(?:\r\n|\r|\n)/g, ' ');
+        csv = csv.concat(newString);         
+      } 
+      
       var hiddenElement = document.createElement('a');
-      hiddenElement.href = 'data:text/tsv;charset=utf-8,' + encodeURIComponent(toWrite);
+      hiddenElement.href = 'data:text/tsv;charset=utf-8,' + encodeURIComponent(csv);
       hiddenElement.target = '_blank';
       hiddenElement.download = 'reports.tsv';
       hiddenElement.click();
+      
       $modalInstance.close();
     }, function(error){ //error function
       console.log(error);
