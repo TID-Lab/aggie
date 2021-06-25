2886b67aa2f607a000df67ed7bb43fac43efa5bf
Max Karpawich <maxkarpawich@gmail.com>
2021-06-25 14:34:45 -0400

    feat: remove obsolete Settings

--- public/angular/js/controllers/settings/settings.js
+++ public/angular/js/controllers/settings/settings.js
@@ -10,44 +10,11 @@ angular.module('Aggie')
   '$timeout',
   '$filter',
   'FlashService',
-  'apiSettingsOptions',
-  'widgetSettingsOptions',
   'Socket',
   'StatsCache',
-  function($scope, $rootScope, $window, Settings, UpdateCTList, Source, $timeout, $filter, flash, apiSettingsOptions, widgetSettingsOptions, Socket, StatsCache) {
-
-    $scope.setting = {};
-
-    apiSettingsOptions.forEach(getSetting);
-    widgetSettingsOptions.forEach(getSetting);
-
-    $scope.inHttp = $window.location.protocol === 'http:';
-    $scope.apiSettingOptions = apiSettingsOptions;
-    $scope.widgetSettingOptions = widgetSettingsOptions;
-    $scope.showCTButton = false;
-
-    $scope.checkSource = function(settings) {
-      Source.getAll().$promise
-      .then(function(response) {
-        for (var i = 0; i < response.length; i++) {
-          if (response[i].media === 'facebook' || response[i].media === 'instagram') {
-            $scope.showCTButton = true;
-          };
-        }
-      });
-    }
-
-    $scope.updateCTList = function() {
-      $rootScope.disableCTButton = true;
-      flash.setNoticeNow('settings.ctUpdate.pending', {persist: true});
-      UpdateCTList.update().$promise
-      .then(function() { flash.setNoticeNow('settings.ctUpdate.success') })
-      .catch(function() { flash.setAlertNow('settings.ctUpdate.failure') })
-      .finally(function() { $rootScope.disableCTButton = false });
-    }
+  function($scope, $rootScope, $window, Settings, UpdateCTList, Source, $timeout, $filter, flash, Socket, StatsCache) {
 
     var init = function() {
-      $scope.checkSource();
       $scope.stats = StatsCache.get('stats');
       Socket.on('stats', updateStats);
       Socket.join('stats');
@@ -58,16 +25,6 @@ angular.module('Aggie')
       $scope.stats = stats;
     };
 
-    function getSetting(name) {
-      Settings.get(name, function success(data) {
-        $scope.setting[data.setting] = angular.copy(data[data.setting]);
-      }, failure);
-    }
-
-    function failure(data) {
-      flash.setAlertNow('settings.error');
-      console.log('failure: ', data);
-    }
     $scope.$on('$destroy', function() {
       Socket.leave('stats');
       Socket.removeAllListeners('stats');
