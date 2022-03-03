83faf0e6993b0d00e2c73786d15dc18b5303bf7c
limmabean <mlim45@gatech.edu>
2022-03-02 23:50:56 -0500

    Front-end works

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import {QueryClient, QueryClientProvider, useQuery, useQueryClient} from "react-query";
 import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
 
 const queryClient = new QueryClient()
 ReactDOM.render((
