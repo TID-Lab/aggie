e18773c375b47aa6107d684982b67c7921192cdd
Sandeep Prasad <s3g007@gmail.com>
2022-06-02 13:51:45 -0400

    Added validations for tag, user, credential. Also added browser check.

--- backend/api/controllers/userController.js
+++ backend/api/controllers/userController.js
@@ -1,61 +1,79 @@
 // Handles CRUD requests for users.
 var User = require('../../models/user');
 var mailer = require('../../mailer');
-const passport = require("passport");
+const passport = require('passport');
+const validator = require('validator');
 
 const sendEmail = (user, req, callback) => {
   // Only send email if not in test mode.
-  if (process.env.NODE_ENV == 'test')
-    callback(null);
+  if (process.env.NODE_ENV == 'test') callback(null);
   else {
     const token = password.encodeToken(user);
-    mailer.sendFromTemplate({
-      template: 'newUser',
-      user: user,
-      token: token,
-      host: req.headers.host,
-      protocol: req.protocol,
-      acceptLanguage: req.headers['accept-language']
-    }, (err) => {
-      if (err) callback(err);
-      else callback(null);
-    });
+    mailer.sendFromTemplate(
+      {
+        template: 'newUser',
+        user: user,
+        token: token,
+        host: req.headers.host,
+        protocol: req.protocol,
+        acceptLanguage: req.headers['accept-language'],
+      },
+      (err) => {
+        if (err) callback(err);
+        else callback(null);
+      }
+    );
   }
-}
+};
 
 exports.user_users = (req, res) => {
-  User.find({}, function(err, users) {
+  User.find({}, function (err, users) {
     if (err) res.status(err.status).send(err.message);
     else res.status(200).send(users);
   });
-}
+};
 
-  // Get a User by id
+// Get a User by id
 exports.user_detail = (req, res) => {
-  User.findById(req.params._id, '-password', function(err, user) {
+  User.findById(req.params._id, '-password', function (err, user) {
     if (err) res.status(err.status).send(err.message);
     else if (!user) res.sendStatus(404);
     else res.status(200).send(user);
   });
-}
+};
 
 // Create a new User
 exports.user_create = (req, res) => {
-  console.log("Attempting to register user with username: " + req.body.username + " and email: " + req.body.email + ".")
-  User.register({
-    username: req.body.username,
-    email: req.body.email,
-  }, req.body.password, (err, user) => {
-    if (err) {
-      res.status(err.status).send(err.message);
-    } else {
-      const authenticate = User.authenticate();
-      authenticate(req.body.username, req.body.password, (err, result) => {
-        if (err) res.status(err.status).send(error.message)
-        else res.status(200).send(user);
-      })
-    }
-  });
+  console.log(
+    'Attempting to register user with username: ' +
+      req.body.username +
+      ' and email: ' +
+      req.body.email +
+      '.'
+  );
+
+  if (!validator.isEmail(req.body.email)) {
+    res.status(400).send('Please provide a valid email.');
+  } else {
+    User.register(
+      {
+        username: req.body.username,
+        email: req.body.email,
+      },
+      req.body.password,
+      (err, user) => {
+        if (err) {
+          res.status(err.status).send(err.message);
+        } else {
+          const authenticate = User.authenticate();
+          authenticate(req.body.username, req.body.password, (err, result) => {
+            if (err) res.status(err.status).send(error.message);
+            else res.status(200).send(user);
+          });
+        }
+      }
+    );
+  }
   /*
   User.register(req.body, function(err, user) {
     err = Error.decode(err);
@@ -68,15 +86,20 @@ exports.user_create = (req, res) => {
       });
     }
   });*/
-}
+};
 
-  // Update a User
+// Update a User
 exports.user_update = (req, res) => {
   User.findById(req.params._id, (err, user) => {
     if (err) return res.status(err.status).send(err.message);
     if (!user) return res.sendStatus(404);
     // Only admin can update users other than itself
-    if (req.user && !(new UserPermissions(req.user)).can('admin users') && req.params._id != req.user._id) return res.send(403);
+    if (
+      req.user &&
+      !new UserPermissions(req.user).can('admin users') &&
+      req.params._id != req.user._id
+    )
+      return res.send(403);
 
     for (var attr in req.body) {
       user[attr] = req.body[attr];
@@ -87,9 +110,9 @@ exports.user_update = (req, res) => {
       else res.status(200).send(user);
     });
   });
-}
+};
 
-  // Delete a User
+// Delete a User
 exports.user_delete = (req, res) => {
   User.findById(req.params._id, (err, user) => {
     if (err) return res.status(err.status).send(err.message);
@@ -100,7 +123,7 @@ exports.user_delete = (req, res) => {
       else res.sendStatus(200);
     });
   });
-}
+};
 
 // Use passport.authenticate() as route middleware to authenticate the request
 exports.user_login = (req, res) => {
@@ -108,15 +131,12 @@ exports.user_login = (req, res) => {
     if (err) res.status(err.status).send(err.message);
     if (!user) res.sendStatus(403);
     res.sendStatus(200);
-  })
-}
+  });
+};
 // Log the user out
 exports.user_logout = (req, res, next) => {
   req.logout();
-}
+};
 
 // Return the currently logged-in user object
-exports.user_session = (req, res) => {
-
-}
-
+exports.user_session = (req, res) => {};
--- src/App.tsx
+++ src/App.tsx
@@ -2,105 +2,153 @@ import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@yaireo/tagify/dist/tagify.css';
-import AggieNavbar from "./components/AggieNavbar";
-import AlertService, {AlertContent} from "./components/AlertService";
-import {Navigate, Route, Routes, useLocation, useNavigate} from "react-router-dom";
-import ReportsIndex from "./pages/report/ReportsIndex";
-import ReportDetails from "./pages/report/ReportDetails";
-import GroupsIndex from "./pages/group/GroupsIndex";
-import GroupDetails from "./pages/group/GroupDetails";
-import SourcesIndex from "./pages/source/SourcesIndex";
-import SourceDetails from "./pages/source/SourceDetails";
-import UsersIndex from "./pages/user/UsersIndex";
-import UserProfile from "./pages/user/UserProfile";
-import TagsIndex from "./pages/tag/TagsIndex";
-import Configuration from "./pages/Configuration";
-import CredentialsIndex from "./pages/CredentialsIndex";
-import Login from "./pages/Login";
-import Analysis from "./pages/Analysis";
-import NotFound from "./pages/NotFound";
-import ResetPassword from "./pages/ResetPassword";
-import {useQuery, useQueryClient} from "react-query";
-import {getSession} from "./api/session";
-import {AxiosError} from "axios";
-import {Session} from "./objectTypes";
-import RelevantReportsIndex from "./pages/report/RelevantReportsIndex";
-import {getAllGroups} from "./api/groups";
+import AggieNavbar from './components/AggieNavbar';
+import AlertService, { AlertContent } from './components/AlertService';
+import {
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from 'react-router-dom';
+import ReportsIndex from './pages/report/ReportsIndex';
+import ReportDetails from './pages/report/ReportDetails';
+import GroupsIndex from './pages/group/GroupsIndex';
+import GroupDetails from './pages/group/GroupDetails';
+import SourcesIndex from './pages/source/SourcesIndex';
+import SourceDetails from './pages/source/SourceDetails';
+import UsersIndex from './pages/user/UsersIndex';
+import UserProfile from './pages/user/UserProfile';
+import TagsIndex from './pages/tag/TagsIndex';
+import Configuration from './pages/Configuration';
+import CredentialsIndex from './pages/CredentialsIndex';
+import Login from './pages/Login';
+import Analysis from './pages/Analysis';
+import NotFound from './pages/NotFound';
+import ResetPassword from './pages/ResetPassword';
+import { useQuery, useQueryClient } from 'react-query';
+import { getSession } from './api/session';
+import { AxiosError } from 'axios';
+import { Session } from './objectTypes';
+import RelevantReportsIndex from './pages/report/RelevantReportsIndex';
+import { getAllGroups } from './api/groups';
+import { Button, Modal } from 'react-bootstrap';
 //TODO: BIG TODO is to correctly type all of react-query usage. Its not critical for function, but it is good for clarity in development.
 //TODO: Also BIG TODO is to ensure EVERY API call has a way of surfacing an error message. I want readble UI alerts but at least console.errors.
+const isSafari = () =>
+  /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
 const App = () => {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
   const location = useLocation();
   let sessionFetching = true;
-  const sessionQuery = useQuery<Session | undefined, AxiosError>("session", getSession, {
-    onError: (err: AxiosError) => {
-      if (err.response && err.response.status === 401) {
-        sessionFetching = false;
-        navigate('/reports');
-      }
-    },
-    onSuccess: data => {
-      sessionFetching = true
-      if (location.pathname === "/login") {
-        navigate('/reports');
-        // For some reason, when there is a navigation here, the session sometimes doesn't tell the navbar it's signed in. Refetching on navigation fixes that.
-        sessionQuery.refetch();
-      }
+  const sessionQuery = useQuery<Session | undefined, AxiosError>(
+    'session',
+    getSession,
+    {
+      onError: (err: AxiosError) => {
+        if (err.response && err.response.status === 401) {
+          sessionFetching = false;
+          navigate('/reports');
+        }
+      },
+      onSuccess: (data) => {
+        sessionFetching = true;
+        if (location.pathname === '/login') {
+          navigate('/reports');
+          // For some reason, when there is a navigation here, the session sometimes doesn't tell the navbar it's signed in. Refetching on navigation fixes that.
+          sessionQuery.refetch();
+        }
+      },
+      retry: sessionFetching,
+    }
+  );
 
-    },
-    retry: sessionFetching
-  });
-
-  queryClient.prefetchQuery('all-groups', getAllGroups)
+  queryClient.prefetchQuery('all-groups', getAllGroups);
   // This is how we "globalize" an alert. This is only for alerts that should remain over multiple views
   const [globalAlert, setGlobalAlert] = useState<AlertContent>({
-    heading: "",
-    message: "",
-    variant: "primary"
+    heading: '',
+    message: '',
+    variant: 'primary',
   });
-  return (
-      <>
-        <AggieNavbar isAuthenticated={sessionQuery.isSuccess} session={sessionQuery.data}/>
-        <AlertService globalAlert={globalAlert} setGlobalAlert={setGlobalAlert}/>
-        <Routes>
-          <Route path="/">
-            {(sessionQuery.isSuccess || sessionQuery.isFetching) &&
-                <>
-                  <Route index element={<Navigate to={'reports'}/>}/>
-                  <Route path="login" element={<Login/>}/>
-                  <Route path='reports' element={<ReportsIndex setGlobalAlert={setGlobalAlert}/>}/>
-                  <Route path='report/:id' element={<ReportDetails/>}/>
-                  <Route path='relevant-reports' element={<RelevantReportsIndex setGlobalAlert={setGlobalAlert}/>}/>
-                  <Route path='groups' element={<GroupsIndex/>}/>
-                  <Route path='group/:id' element={<GroupDetails/>}/>
-                  <Route path='sources' element={<SourcesIndex/>}/>
-                  <Route path='source/:id' element={<SourceDetails/>}/>
-                  <Route path='users' element={<UsersIndex/>}/>
-                  <Route path='user/:id' element={<UserProfile session={sessionQuery.data}/>}/>
-                  <Route path='tags' element={<TagsIndex/>}/>
-                  <Route path='config' element={<Configuration/>}/>
-                  <Route path='credentials' element={<CredentialsIndex/>}/>
-                  <Route path='analysis' element={<Analysis/>}/>
-                  <Route path='reset-password' element={<ResetPassword/>}/>
-                  <Route path='404' element={<NotFound/>}/>
-                  <Route path="*" element={<Navigate replace to="/404"/>}/>
-                </>
-            }
-            {/*@ts-ignore*/}
-            {sessionQuery.isError && sessionQuery.error && sessionQuery.error.response && sessionQuery.error.response.status === 401 &&
-                <>
-                  <Route path='reset-password' element={<ResetPassword/>}/>
-                  <Route path="login" element={<Login/>}/>
-                  <Route path="*" element={<Navigate replace to="login"/>}/>
-                </>
-            }
-          </Route>
-        </Routes>
-      </>
-  )
 
-}
+  const InitialApp = (
+    <>
+      <AggieNavbar
+        isAuthenticated={sessionQuery.isSuccess}
+        session={sessionQuery.data}
+      />
+      <AlertService globalAlert={globalAlert} setGlobalAlert={setGlobalAlert} />
+      <Routes>
+        <Route path='/'>
+          {(sessionQuery.isSuccess || sessionQuery.isFetching) && (
+            <>
+              <Route index element={<Navigate to={'reports'} />} />
+              <Route path='login' element={<Login />} />
+              <Route
+                path='reports'
+                element={<ReportsIndex setGlobalAlert={setGlobalAlert} />}
+              />
+              <Route path='report/:id' element={<ReportDetails />} />
+              <Route
+                path='relevant-reports'
+                element={
+                  <RelevantReportsIndex setGlobalAlert={setGlobalAlert} />
+                }
+              />
+              <Route path='groups' element={<GroupsIndex />} />
+              <Route path='group/:id' element={<GroupDetails />} />
+              <Route path='sources' element={<SourcesIndex />} />
+              <Route path='source/:id' element={<SourceDetails />} />
+              <Route path='users' element={<UsersIndex />} />
+              <Route
+                path='user/:id'
+                element={<UserProfile session={sessionQuery.data} />}
+              />
+              <Route path='tags' element={<TagsIndex />} />
+              <Route path='config' element={<Configuration />} />
+              <Route path='credentials' element={<CredentialsIndex />} />
+              <Route path='analysis' element={<Analysis />} />
+              <Route path='reset-password' element={<ResetPassword />} />
+              <Route path='404' element={<NotFound />} />
+              <Route path='*' element={<Navigate replace to='/404' />} />
+            </>
+          )}
+          {/*@ts-ignore*/}
+          {sessionQuery.isError &&
+            sessionQuery.error &&
+            sessionQuery.error.response &&
+            sessionQuery.error.response.status === 401 && (
+              <>
+                <Route path='reset-password' element={<ResetPassword />} />
+                <Route path='login' element={<Login />} />
+                <Route path='*' element={<Navigate replace to='login' />} />
+              </>
+            )}
+        </Route>
+      </Routes>
+    </>
+  );
 
+  const WrongBrowser = (
+    <Modal.Dialog color='danger'>
+      <Modal.Header>
+        <Modal.Title>:(</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <p>
+          Hi, unfortunately Aggie does not support Safari yet. Please use Google
+          Chrome, Firefox or Edge browser to run Aggie.
+        </p>
+      </Modal.Body>
+    </Modal.Dialog>
+  );
+
+  const AppToRender = isSafari() ? WrongBrowser : InitialApp;
+
+  return AppToRender;
+};
 
 export default App;
